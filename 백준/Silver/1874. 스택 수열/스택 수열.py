3ce107e0581aef1e044d4a54146b61b92abe8ada
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2023-11-20 18:39:12 +0900

    [Silver II] Title: 스택 수열, Time: 764 ms, Memory: 119016 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -12,17 +12,26 @@ result = []
 for i in range(0, n):
     input = int(s.readline().rstrip())
 
-    while item <= input:
-        stack.append(item)
-        item += 1
-        result.append("+")
-
-    if stack[-1] == input:
+    if input in stack: #stack이 무조건 하나 이상 있음
+        stackTop = stack[-1]
+        while stackTop != input:
+            stack.pop()
+            result.append("-")
+            stackTop = stack[-1]
         stack.pop()
         result.append("-")
-    else:
-        print("NO")
-        break
+    else: #input이 스택에 없음
+        if item > input:
+            print("NO")
+            break
+        else: #item <= input 이므로 추가하면 됨
+            differ = input-item
+            for i in range(0, differ+1):
+                stack.append(item)
+                item += 1
+                result.append("+")
+            stack.pop()
+            result.append("-")
 else:
     for i in result:
         print(i)
