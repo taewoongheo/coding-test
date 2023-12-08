a06cfa02dc7d426882c5f50df6a27347e6e76a04
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2023-12-08 17:05:03 +0900

    [Gold IV] Title: 오큰수, Time: 1068 ms, Memory: 155536 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,13 +1,14 @@
 from sys import stdin as s
 
 # input.txt file
+#s = open('input.txt')
 
 n = int(s.readline().rstrip())
 arr = list(map(int, s.readline().split()))
 stack = []
 answer = [-1] * n
 
-for i in range(0, n):
+for i in range(0, len(arr)):
     while stack and arr[stack[-1]] < arr[i]:
         answer[stack.pop()] = arr[i]
     stack.append(i)
