d7c6e0d63d3dad8446adf3b29be23840ed99fad1
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2023-12-19 16:20:15 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 84 ms, Memory: 12064 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -6,7 +6,7 @@ public class Main {
     public static int min = Integer.MAX_VALUE;
 
     public static void bt(int depth, int[] num, int N, int cal, int[] operator) {
-        if (depth == N) { // 끝 도달. max와 min이랑 비교해서 최댓값 최솟값 찾기
+        if (depth == N) { //깊이(depth)가 맨 아래(N)까지 들어오면 최소, 최댓값 갱신
             if (cal > max) {
                 max = cal;
             }
@@ -17,19 +17,19 @@ public class Main {
         }
 
         for (int i = 0; i < 4; i++) {
-            if (operator[i] > 0) {
-                int temp = cal; // 현재 연산 결과 임시 저장
-                switch (i) { // 연산자에 따라 계산
-                    case 0:
+            if (operator[i] > 0) { //pruning. 연산자의 개수가 1개 이상일 때만 통과
+                int temp = cal; //현재 레벨의 계산값 저장
+                switch (i) {
+                    case 0: //+
                         cal += num[depth];
                         break;
-                    case 1:
+                    case 1: //-
                         cal -= num[depth];
                         break;
-                    case 2:
+                    case 2: //*
                         cal *= num[depth];
                         break;
-                    case 3:
+                    case 3: //÷
                         int inputNum = num[depth];
                         if (inputNum < 0) {
                             cal = cal / (inputNum * -1);
@@ -40,11 +40,10 @@ public class Main {
                         break;
                 }
 
-                operator[i]--;
+                operator[i]--; //사용한 연산자 개수 하나 뺌
                 bt(depth + 1, num, N, cal, operator);
-                // 백트래킹: 재귀 호출 이후 원래의 연산 결과로 복구
-                cal = temp;
-                operator[i]++;
+                cal = temp; //값 복구
+                operator[i]++; //연산자 배열 값 복구
             }
         }
     }
@@ -62,9 +61,9 @@ public class Main {
             num[i] = Integer.parseInt(inputNum[i]);
         }
 
-        int cal = num[0];
+        int cal = num[0]; //cal=계산한 값을 저장
         for (int i = 0; i < 4; i++) {
-            operator[i] = Integer.parseInt(inputOper[i]);
+            operator[i] = Integer.parseInt(inputOper[i]); //연산자 개수 초기화
         }
 
         bt(1, num, N, cal, operator);
