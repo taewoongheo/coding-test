477bddd52b591609cc90d27854ec993cfa44f651
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-01-16 13:17:17 +0900

    [Silver II] Title: 스택 수열, Time: 496 ms, Memory: 118032 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,28 +1,35 @@
-from sys import stdin as s
+import sys
+#stdin = open("input.txt")
 
-# input.txt file
-#s = open('input.txt')
-
-n = int(s.readline().rstrip())
+n = int(sys.stdin.readline().rstrip())
 
 item = 1
-stack = []
 result = []
+stack = []
+falseSwitch = False
 
 for i in range(0, n):
-    input = int(s.readline().rstrip())
-
-    while item <= input:
-        stack.append(item)
-        item += 1
-        result.append("+")
-
-    if stack[-1] == input:
+    num = int(sys.stdin.readline().rstrip())
+    if item <= num:
+        for j in range(0, num-item+1):
+            stack.append(item)
+            result.append("+")
+            item += 1
         stack.pop()
         result.append("-")
-    else:
-        print("NO")
-        break
+    elif item > num:
+        if num in stack:
+            while True:
+                popItem = stack.pop()
+                result.append("-")
+                if popItem == num:
+                    break
+        else:
+            falseSwitch = True
+
+if (falseSwitch):
+    print("NO")
 else:
     for i in result:
         print(i)
+
