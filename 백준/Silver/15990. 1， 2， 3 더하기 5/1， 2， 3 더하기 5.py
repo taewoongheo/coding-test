de2ead1f81a3354abb3ecd03d05ef3b940d2255f
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-01-18 14:38:53 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 172 ms, Memory: 51356 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -1,20 +1,24 @@
-from sys import stdin as s
-#s = open("input.txt")
+import sys
 
-N = int(s.readline().rstrip())
+#stdin = open("input.txt")
 
-dp = [[0, 0, 0, 0] for _ in range(100001)]
+n = int(sys.stdin.readline().rstrip())
 
-mod = 1000000009
+dp = [[0, 0, 0] for _ in range(0, 100001)]
 
-dp[1] = [0, 1, 0, 0]
-dp[2] = [0, 0, 1, 0]
-dp[3] = [0, 1, 1, 1]
+dp[1] = [1, 0, 0]
+dp[2] = [0, 1, 0]
+dp[3] = [1, 1, 1]
 
 for i in range(4, 100001):
-    dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % mod
-    dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % mod
-    dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % mod
+    dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % 1000000009
+    dp[i][1] = (dp[i-2][0] + dp[i-2][2]) % 1000000009
+    dp[i][2] = (dp[i-3][0] + dp[i-3][1]) % 1000000009
 
-for i in range(N):
-    print(sum(dp[int(s.readline().rstrip())])%mod)
+for i in range(n):
+    num = int(sys.stdin.readline().rstrip())
+    sum = 0
+    for j in range(3):
+        sum += dp[num][j]
+    print(sum % 1000000009)
+
