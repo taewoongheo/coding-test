ce5847b50032205d4756508d62dc81dc741737a4
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-01-19 14:18:49 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,16 +1,16 @@
-from sys import stdin as s
-#s = open("input.txt")
+import sys
 
-N = int(s.readline().rstrip())
+#stdin = open("input.txt")
 
-dp = [[0] * 10 for _ in range(N)]
+n = int(sys.stdin.readline().rstrip())
+
+dp = [[0] * 10 for _ in range(n+1)]
 dp[0] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-for i in range(1, N):
+for i in range(1, n+1):
     dp[i][0] = dp[i-1][1]
     dp[i][9] = dp[i-1][8]
+    for j in range(1, 9):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
-    for k in range(1, 9):
-        dp[i][k] = dp[i-1][k-1] + dp[i-1][k+1]
-
-print(sum(dp[N-1]) % 1000000000)
+print(sum(dp[n-1]) % 1000000000)
