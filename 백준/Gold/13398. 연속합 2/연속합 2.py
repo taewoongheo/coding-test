4b53c21a4a81bf51660a32df261af123d5b15d3b
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-02-13 14:51:48 +0900

    [Gold V] Title: 연속합 2, Time: 168 ms, Memory: 42828 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -2,27 +2,27 @@ import sys
 
 #stdin = open("input.txt")
 
-T = int(sys.stdin.readline().rstrip())
+n = int(sys.stdin.readline().rstrip())
 arr = list(map(int, sys.stdin.readline().split(" ")))
 
-dp = [[0] * 2 for _ in range(T)]
-dp[0][0] = arr[0]
-dp[0][1] = arr[0]
-if T > 1:
+dp = [[0, 0] for _ in range(n)]
+
+# 첫번째 인덱스: 중간 삭제 x
+# 두번째 인덱스: 중간 삭제 o
+dp[0] = [arr[0], arr[0]]
+if n > 1:
     dp[1][0] = max(dp[0][0] + arr[1], arr[1])
     dp[1][1] = arr[1]
 
-# 0번 인덱스 - 특정 원소 제거 x
-# 1번 인덱스 - 특정 원소 제거 o
-for i in range(2, T):
-    dp[i][0] = max(arr[i], dp[i-1][0] + arr[i])
-    dp[i][1] = max(dp[i-1][1], dp[i-2][0]) + arr[i]
+for i in range(2, n):
+    dp[i][0] = max(dp[i-1][0] + arr[i], arr[i])
+    dp[i][1] = max(dp[i-2][0], dp[i-1][1]) + arr[i]
 
-result = dp[0][0]
-for i in range(T):
-    if result < dp[i][0]:
-        result = dp[i][0]
-    if result < dp[i][1]:
-        result = dp[i][1]
+maxValue = dp[0][0]
+for i in range(n):
+    if dp[i][0] > maxValue:
+        maxValue = dp[i][0]
+    if dp[i][1] > maxValue:
+        maxValue = dp[i][1]
 
-print(result)
+print(maxValue)
