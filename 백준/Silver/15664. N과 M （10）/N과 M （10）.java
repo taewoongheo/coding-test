5b892c7750c8c8bb473185443e71ff803252d24f
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-02-19 01:19:23 +0900

    [Silver II] Title: N과 M (10), Time: 80 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static int[] arr;
+    public static boolean[] visited;
+    public static int[] intArr;
+    public static BufferedWriter bw;
+    public static int N, M;
+
+    public static void dfs(int depth, int lastIdxNum) throws IOException {
+        if (M == depth) {
+            for (int i = 0; i < M; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+        } else {
+            int before = 0;
+            for (int i = 0; i < N; i++) {
+                if (visited[i]) {
+                    continue;
+                }
+                if ((before != intArr[i]) && (lastIdxNum <= intArr[i])) {
+                    arr[depth] = intArr[i];
+                    visited[i] = true;
+                    before = intArr[i];
+                    lastIdxNum = intArr[i];
+                    dfs(depth + 1, lastIdxNum);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] inputNM = br.readLine().split(" ");
+        String[] inputArr = br.readLine().split(" ");
+        N = Integer.parseInt(inputNM[0]);
+        M = Integer.parseInt(inputNM[1]);
+        intArr = new int[N];
+        for (int i = 0; i < N; i++) {
+            intArr[i] = Integer.parseInt(inputArr[i]);
+        }
+        visited = new boolean[N];
+        arr = new int[M];
+        Arrays.sort(intArr);
+
+        dfs(0, 0);
+
+        bw.flush();
+    }
+}
