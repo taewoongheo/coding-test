c9f5cf3a7e4713dfbcd7c482eb602dc88b9edc65
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-02-25 12:49:08 +0900

    [Silver II] Title: N과 M (9), Time: 220 ms, Memory: 33308 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,26 +1,33 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
-    public static ArrayList<int[]> resultArr;
-    public static int[] intArr;
     public static BufferedWriter bw;
     public static int N, M;
+    public static int[] arr;
+    public static int[] result;
+    public static boolean[] visited;
 
-    public static void bt(int depth, int[] arr, Boolean[] boolArr) {
+    public static void dfs(int depth) throws IOException {
         if (depth == M) {
-            int[] copy = arr.clone();
-            resultArr.add(copy);
+            for (int i = 0; i < M; i++) {
+                bw.write(result[i] + " ");
+            }
+            bw.write("\n");
             return;
         }
+        int before = 0;
         for (int i = 0; i < N; i++) {
-            if (!boolArr[i]) {
-                boolArr[i] = true;
-                arr[depth] = intArr[i];
-                bt(depth + 1, arr, boolArr);
-                boolArr[i] = false;
+            if (visited[i]) {
+                continue;
+            }
+            if (before != arr[i]) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                before = arr[i];
+                dfs(depth + 1);
+                visited[i] = false;
             }
         }
     }
@@ -29,50 +36,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] inputNM = br.readLine().split(" ");
+        String[] NM = br.readLine().split(" ");
+        N = Integer.parseInt(NM[0]);
+        M = Integer.parseInt(NM[1]);
         String[] inputArr = br.readLine().split(" ");
-        N = Integer.parseInt(inputNM[0]);
-        M = Integer.parseInt(inputNM[1]);
-        intArr = new int[N];
+        arr = new int[N];
         for (int i = 0; i < N; i++) {
-            intArr[i] = Integer.parseInt(inputArr[i]);
-        }
-        Arrays.sort(intArr);
-
-        resultArr = new ArrayList<>();
-        Boolean[] boolArr = new Boolean[N];
-        for (int j = 0; j < N; j++) {
-            boolArr[j] = false;
-        }
-        for (int i = 0; i < N; i++) {
-            int[] arr = new int[M];
-            boolArr[i] = true;
-            arr[0] = intArr[i];
-            bt(1, arr, boolArr);
-            boolArr[i] = false;
+            arr[i] = Integer.parseInt(inputArr[i]);
         }
 
-        //print resultArr
-        int[] lastArr = new int[M];
-        for (int i = 0; i < M; i++) {
-            lastArr[i] = 0;
-        }
+        Arrays.sort(arr);
 
-        for (int i = 0; i < resultArr.size(); i++) {
-            boolean isDupli = false;
-            for (int j = i+1; j < resultArr.size(); j++) {
-                if (Arrays.equals(resultArr.get(i), resultArr.get(j))) {
-                    isDupli = true;
-                    break;
-                }
-            }
-            if (!isDupli) {
-                for (int l = 0; l < M; l++) {
-                    bw.write(resultArr.get(i)[l] + " ");
-                }
-                bw.write("\n");
-            }
-        }
+        result = new int[M];
+        visited = new boolean[N];
+        dfs(0);
+
         bw.flush();
     }
 }
