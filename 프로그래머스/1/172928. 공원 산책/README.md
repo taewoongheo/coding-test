9983558d70629cc3ecbee65832885e460816e633
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-03-13 09:50:06 +0900

    [level 1] Title: 공원 산책, Time: 10.39 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
 
 ### 성능 요약
 
-메모리: 88.1 MB, 시간: 10.28 ms
+메모리: 80.2 MB, 시간: 10.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 12일 20:11:56
+2024년 03월 13일 09:50:03
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -2,97 +2,71 @@ import java.util.*;
 
 class Solution {
     
+    public static int[][] dirArr = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
     public int[] solution(String[] park, String[] routes) {
-        
-        String[][] parkTable = new String[park.length][park[0].length()];
-        // System.out.println("park.length: "+park.length+" park[0].length(): "+park[0].length());
         
         int x = 0; 
         int y = 0;
+        boolean start = false;
         for(int i = 0; i < park.length; i++) {
-            String parkLine = park[i];
-            for(int j = 0; j < parkLine.length(); j++) {
-                parkTable[i][j] = String.valueOf(parkLine.charAt(j));
-                if (parkTable[i][j].equals("S")) {
-                    x = i;
-                    y = j;
-                } 
+            if(start) {
+                break;
             }
-        }
-        
-        for(int i = 0; i < park.length; i++) {
-            // System.out.println(Arrays.toString(parkTable[i]));
+            for(int j = 0; j < park[0].length(); j++) {
+                if(park[i].charAt(j) == 'S') {
+                    x = i; 
+                    y = j;
+                    start = true;
+                    break;
+                }
+            }
         }
         
         System.out.println(x+" "+y);
-        int h = x; 
-        int w = y; 
-        for(int i = 0; i < routes.length; i++) {
-            String[] command = routes[i].split(" ");
-            String direction = command[0];
-            int distance = Integer.parseInt(command[1]);
-            h = x; //임시 높이 좌표(x)
-            w = y; //임시 넓이 좌표(y)
+        
+        for(String cell : routes) {
+            String[] command = cell.split(" ");
+            int dirIdx = direction(command[0]);
+            int dis = Integer.parseInt(command[1]);
             
-            //동서남북에 따라 이동
-            if (direction.equals("E")) {
-                // System.out.println("E");
-                w += distance;
-            } else if(direction.equals("W")) {
-                // System.out.println("W");
-                w -= distance;
-            } else if(direction.equals("S")) {
-                // System.out.println("S");
-                h += distance;
-            } else if(direction.equals("N")) {
-                // System.out.println("N");
-                h -= distance;
+            if(isWalk(park, x, y, dirIdx, dis)) {
+                x += dis*dirArr[dirIdx][0];
+                y += dis*dirArr[dirIdx][1];
             }
             
-            // System.out.println("x: "+x+" y: "+y+" h: "+h+" w: "+w);
-            if (((w >= park[0].length()) || (w < 0)) || ((h >= park.length) || (h < 0))) {
-                // System.out.println("넘침: "+w+" "+h);
-                h = x; 
-                w = y;
-            }
-            
-            // System.out.println("x: "+x+" y: "+y+" h: "+h+" w: "+w);
-            for(int j = y+1; j <= w; j++) {
-                if (parkTable[x][j].equals("X")) {
-                    w = y;
-                    break;
-                }
-            }
-            
-            for(int j = y-1; j >= w; j--) {
-                if (parkTable[x][j].equals("X")) {
-                    w = y;
-                    break;
-                }
-            }
-            
-            for(int j = x+1; j <= h; j++) {
-                if (parkTable[j][y].equals("X")) {
-                    h = x;
-                    break;
-                }
-            }
-            
-            for(int j = x-1; j >= h; j--) {
-                if (parkTable[j][y].equals("X")) {
-                    h = x;    
-                    break;
-                }
-            }
-                
-            x = h;
-            y = w;
-            // System.out.println("x: "+x+" y: "+y);
-            // System.out.println("-------");
         }
         
-        int[] answer = {x, y};
+        return new int[]{x, y};
+    }
+    
+    public static int direction(String dir) {
+        int idx = 0;  
         
-        return answer;
+        if (dir.equals("E")) {
+            idx = 0; 
+        } else if(dir.equals("W")) {
+            idx = 1;
+        } else if(dir.equals("S")) {
+            idx = 2;
+        } else if(dir.equals("N")) {
+            idx = 3;
+        }
+        return idx;
+    }
+    
+    public static boolean isWalk(String[] park, int x, int y, int dirIdx, int dis) {
+
+        for(int i = 0; i < dis; i++) {
+            x += dirArr[dirIdx][0];
+            y += dirArr[dirIdx][1];
+            
+            if( ((x < 0) || (x >= park.length)) || ((y < 0) || (y >= park[0].length())) || park[x].charAt(y)=='X') {
+                System.out.println("실패");
+                return false;
+            }
+        }
+        System.out.println("성공: "+x+" "+y);
+        return true;
     }
 }
