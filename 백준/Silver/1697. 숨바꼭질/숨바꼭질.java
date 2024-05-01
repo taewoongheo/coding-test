f975bcb43b524b9ebaf515bd424441b986f1e59d
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-05-01 15:14:49 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 16000 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -4,74 +4,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 public class Main {
 
     public static int N, K;
-    public static int out = Integer.MAX_VALUE;
-    public static int[] dis;
-    public static Queue<int[]> bfsQueue = new LinkedList<>();
-    public static String[] oper = new String[]{"*", "+", "-"};
+    public static int[] v;
+    public static Queue<Integer> bfsQueue = new LinkedList<>();
 
-    public static void bt(int loc, int diff, int cnt) {
 
-        bfsQueue.add(new int[]{loc, diff, cnt});
+    public static void bfs() {
+
+        bfsQueue.add(N);
+        v[N] = 1;
         while (!bfsQueue.isEmpty()) {
-            int[] curInfo = bfsQueue.poll();
-            int curLoc = curInfo[0];
-            int curDiff = curInfo[1];
-            int curCnt = curInfo[2];
-
-            if ((curLoc == K) && (out > curCnt)) {
-                out = curCnt;
+            int cur = bfsQueue.remove();
+            if (cur == K) {
+                System.out.println(v[cur]-1);
+                return;
             }
-
-            if ((dis[curLoc] < curCnt) && (dis[curLoc] != 0)) {
-                continue;
+            if (cur+1 <= 100000 && v[cur + 1] == 0) {
+                v[cur + 1] = v[cur] + 1;
+                bfsQueue.add(cur + 1);
             }
-            dis[curLoc] = curCnt;
-
-            for (int i = 0; i < 3; i++) {
-                String operator = oper[i];
-                int dupliLoc = curLoc;
-
-                if (operator.equals("+")) {
-                    if (N > K) {
-                        continue;
-                    }
-                    dupliLoc += 1;
-                } else if (operator.equals("-")) {
-                    dupliLoc -= 1;
-                } else if (operator.equals("*")) {
-                    if (N > K) {
-                        continue;
-                    }
-                    dupliLoc *= 2;
-                }
-
-                int dupliLocDiff = Math.abs(dupliLoc - K);
-                if (N > K) {
-                    if (dupliLocDiff < K) {
-                        if (curDiff < dupliLocDiff) {
-                            continue;
-                        }
-                    }
-                } else if (N < K) {
-                    if (dupliLocDiff > K) {
-                        if(curDiff > dupliLocDiff) {
-                            continue;
-                        }
-                    }
-                }
-
-                if (dupliLoc < 0 || dupliLoc > 100000) {
-                    continue;
-                }
-
-                bfsQueue.add(new int[]{dupliLoc, dupliLocDiff, curCnt + 1});
+            if (cur-1 >= 0 && v[cur - 1] == 0) {
+                v[cur - 1] = v[cur] + 1;
+                bfsQueue.add(cur - 1);
             }
-
+            if (cur*2 <= 100000 && v[cur * 2] == 0) {
+                v[cur * 2] = v[cur] + 1;
+                bfsQueue.add(cur * 2);
+            }
         }
     }
 
@@ -80,10 +42,8 @@ public class Main {
         String[] line = br.readLine().split(" ");
         N = Integer.parseInt(line[0]);
         K = Integer.parseInt(line[1]);
-        dis = new int[100001];
+        v = new int[100001];
 
-        bt(N, Math.abs(N - K), 0);
-
-        System.out.println(out);
+        bfs();
     }
 }
