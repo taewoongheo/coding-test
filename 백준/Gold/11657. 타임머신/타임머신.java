3c36cd83ae32d32299f3782722232ae7e3f4e559
Taewoong Heo <127009780+TaewoooongHeo01@users.noreply.github.com>
2024-10-02 10:39:07 +0900

    [Gold IV] Title: 타임머신, Time: 204 ms, Memory: 18240 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -6,35 +6,34 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] line = br.readLine().split(" ");
-        int N = Integer.parseInt(line[0]);
-        int M = Integer.parseInt(line[1]);
-        long[][] edges = new long[M][3];
-        for (int i = 0; i < M; i++) {
+        int V = Integer.parseInt(line[0]);
+        int E = Integer.parseInt(line[1]);
+
+        long[][] edges = new long[E][3];
+        for (int i = 0; i < E; i++) {
             line = br.readLine().split(" ");
-            long sNode = Long.parseLong(line[0]);
-            long eNode =  Long.parseLong(line[1]);
-            long cost = Long.parseLong(line[2]);
-            edges[i] = new long[]{sNode, eNode, cost};
+            int node1 = Integer.parseInt(line[0]);
+            int node2 = Integer.parseInt(line[1]);
+            int cost = Integer.parseInt(line[2]);
+            edges[i] = new long[]{node1, node2, cost};
         }
 
-        //bellman-ford
+        long[] dist = new long[V + 1];
         long INF = Long.MAX_VALUE;
-        long[] dist = new long[N + 1];
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= V; i++) {
             dist[i] = INF;
         }
-        dist[1] = 0; //1번 도시 초기화
-
-        boolean infinite = false; //음수 간선 순환 여부
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        dist[1] = 0;
+        boolean infinite = false;
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < E; j++) {
                 long[] edge = edges[j];
                 long sNode = edge[0];
                 long eNode = edge[1];
                 long cost = edge[2];
                 if (dist[(int) sNode] != INF && dist[(int) sNode] + cost < dist[(int) eNode]) {
-                    dist[(int) eNode] = dist[(int) sNode] + cost;
-                    if (i == N - 1) {
+                    dist[(int)eNode] = dist[(int) sNode] + cost;
+                    if (i == V - 1) {
                         infinite = true;
                         break;
                     }
@@ -45,8 +44,8 @@ public class Main {
         if (infinite) {
             bw.write("-1\n");
         } else {
-            for (int i = 2; i <= N; i++) {
-                bw.write((dist[i] != INF ? dist[i] : "-1") + "\n");
+            for (int i = 2; i <= V; i++) {
+                bw.write((dist[i] == INF ? "-1" : dist[i]) + "\n");
             }
         }
         bw.flush();
