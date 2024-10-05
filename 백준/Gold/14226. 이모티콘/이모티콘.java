f9b58afed17d3eec482f56ca59cd6ba8e1699279
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2024-10-05 20:02:03 +0900

    [Gold IV] Title: 이모티콘, Time: 80 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.java
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -1,67 +1,63 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static int s;
-    public static boolean[][] v;
+	public static class Emo {
+		public int num, time, clipboard;
+		public Emo(int num, int clipboard, int time) {
+			this.num = num;
+			this.clipboard = clipboard;
+			this.time = time;
+		}
+	}
 
-    public static class Emo {
-        public int num;
-        public int clipboard;
-        public int time;
-        public Emo(int num, int clipboard, int time) {
-            this.num = num;
-            this.clipboard = clipboard;
-            this.time = time;
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void bfs() {
-        Queue<Emo> q = new LinkedList<>();
-        v = new boolean[2001][2001];
+		int S = Integer.parseInt(br.readLine());
+		boolean[][] v = new boolean[2001][2001]; //[이모티콘 수][클립보드 수]
 
-        q.offer(new Emo(1, 0, 0));
-        while(!q.isEmpty()) {
-            Emo cur = q.poll();
-            int n = cur.num;
-            int c = cur.clipboard;
-            int t = cur.time;
+		//bfs
+		Emo emo = new Emo(1, 0, 0);
+		Queue<Emo> q = new LinkedList<>();
+		q.offer(emo);
+		v[1][0] = true;
 
-            if (n == s) {
-                System.out.println(t);
-                return;
-            }
+		while (!q.isEmpty()) {
+			Emo cur = q.poll();
+			int n = cur.num;
+			int c = cur.clipboard;
+			int t = cur.time;
 
-            if (n > 0 && n < 2000) {
-                if (!v[n][n]) {
-                    v[n][n] = true;
-                    q.offer(new Emo(n, n, t + 1));
-                }
+			if (n == S) {
+				bw.write(t + "\n");
+				break;
+			}
 
-                if (!v[n - 1][c]) {
-                    v[n - 1][c] = true;
-                    q.offer(new Emo(n - 1, c, t + 1));
-                }
-            }
+			if (n > 0 && n < 2001) {
+				if (!v[n][n]) {
+					v[n][n] = true;
+					q.offer(new Emo(n, n, t + 1));
+				}
+			}
 
-            if (n > 0 && n + c < 2000) {
-                if (!v[n + c][c]) {
-                    v[n + c][c] = true;
-                    q.offer(new Emo(n + c, c, t + 1));
-                }
-            }
-        }
-    }
+			if (n > 0) {
+				if (!v[n - 1][n]) {
+					v[n - 1][n] = true;
+					q.offer(new Emo(n - 1, c, t + 1));
+				}
+			}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        s = Integer.parseInt(br.readLine());
+			if (c != 0 && n + c < 2001) {
+				if (!v[n + c][c]) {
+					v[n + c][c] = true;
+					q.offer(new Emo(n + c, c, t + 1));
+				}
+			}
+		}
 
-        bfs();
-    }
+		bw.flush();
+	}
 }
