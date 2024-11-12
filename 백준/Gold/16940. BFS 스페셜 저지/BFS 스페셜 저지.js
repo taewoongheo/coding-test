35b4af4658b3ec2dc82c598b810576ec3f34b306
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2024-11-12 11:23:19 +0900

    [Gold III] Title: BFS 스페셜 저지, Time: 468 ms, Memory: 82792 KB -BaekjoonHub

--- 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.js
+++ 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.js
@@ -1,3 +1,34 @@
+class Node {
+  constructor(item) {
+    this.item = item;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(item) {
+    const node = new Node(item);
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    this.length += 1;
+  }
+  pop() {
+    const popItem = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+    return popItem.item;
+  }
+}
+
 const input = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
@@ -23,12 +54,12 @@ if (seq[0] !== 1) {
 }
 const v = Array(N + 1).fill(false);
 v[1] = true;
-const q = [];
+const q = new Queue();
 q.push(1);
 
 let idx = 1;
 while (q.length !== 0) {
-  const cNode = q.shift();
+  const cNode = q.pop();
   const neighbors = g.get(cNode);
   let cnt = 0;
   neighbors.forEach((e) => {
