5daa972d163e679e1a9b1d7b33bfe517bcf4492c
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-01-26 12:39:26 +0700

    [Gold IV] Title: 수 묶기, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.js
+++ 백준/Gold/1744. 수 묶기/수 묶기.js
@@ -1,3 +1,5 @@
+
+
 const input = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
   .toString()
@@ -5,23 +7,32 @@ const input = require("fs")
   .split("\n")
   .map(Number);
 
-const N = input.shift();
+input.shift();
 const pos = input.filter((el) => el > 0).sort((a, b) => a - b);
 const neg = input.filter((el) => el <= 0).sort((a, b) => b - a);
 
 let ans = 0;
 while (pos.length !== 0) {
-  const el1 = pos.pop();
-  const el2 = pos.pop();
-  if (!el2) ans += el1;
-  else ans += Math.max(el1 + el2, el1 * el2);
+  const num1 = pos.pop();
+  const num2 = pos.pop();
+
+  if (num2 === undefined) {
+    ans += num1;
+    continue;
+  }
+
+  ans += Math.max(num1 + num2, num1 * num2);
 }
 
 while (neg.length !== 0) {
-  const el1 = neg.pop();
-  const el2 = neg.pop();
-  if (el2 === undefined) ans += el1;
-  else ans += Math.max(el1 + el2, el1 * el2);
+  const num1 = neg.pop();
+  const num2 = neg.pop();
+
+  if (num2 === undefined) {
+    ans += num1;
+    continue;
+  }
+  ans += Math.max(num1 + num2, num1 * num2);
 }
 
 console.log(ans);
