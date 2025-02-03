c03ed24e40bc6529be48e759e4253aa95f501a40
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-03 11:27:44 +0700

    [Silver IV] Title: 숫자 카드 2, Time: 1180 ms, Memory: 161132 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,8 +1,12 @@
 // N은 최대 500000.
 // 질문: 자바스크립트에서 배열의 메모리 계산은 c 와 동일한가?(아닐거같은데)
 
-// 숫자 정보를 어떻게 저장할까?
+// 질문: 숫자 정보를 어떻게 저장할까?
 // Map? 2차원 배열? 어떤게 더 메모리를 많이 잡아먹지?
+
+// 질문: 재귀방식과 루프 방식의 명확한 차이는?
+// 예상: 재귀방식이 메모리를 더 잡아먹을 듯. 왜냐하면 스택이 계속 생기니까.
+// 그럼 루프방식이 항상 더 좋은가?
 
 let [N, narr, M, marr] = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
@@ -41,9 +45,12 @@ for (let i = 0; i < M; i++) {
 console.log(ans.trim());
 
 function binarySearch(sIdx, eIdx, el) {
-  const mIdx = Math.floor((sIdx + eIdx) / 2);
-  if (sIdx > eIdx) return -1;
-  if (numnarr[mIdx][0] === el) return mIdx;
-  else if (numnarr[mIdx][0] > el) return binarySearch(sIdx, mIdx - 1, el);
-  else if (numnarr[mIdx][0] < el) return binarySearch(mIdx + 1, eIdx, el);
+  while (sIdx <= eIdx) {
+    const mIdx = Math.floor((sIdx + eIdx) / 2);
+    if (numnarr[mIdx][0] === el) return mIdx;
+    else if (numnarr[mIdx][0] > el) eIdx = mIdx - 1;
+    else if (numnarr[mIdx][0] < el) sIdx = mIdx + 1;
+  }
+
+  return -1;
 }
