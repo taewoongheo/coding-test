260f1ef4f82cc14f7003f09b4366df976e9af884
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-07 12:06:44 +0700

    [Gold V] Title: 하노이 탑 이동 순서, Time: 980 ms, Memory: 236712 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
@@ -1,20 +1,18 @@
 const N = Number(
   require("fs")
-    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+    .readFileSync(process.platform === "linux" ? "dev/stdin" : "input.txt")
     .toString()
     .trim()
 );
 
-let ans = 0;
-let ansArr = [];
+const result = [];
 hanoi(N, 1, 3, 2);
-console.log(ans);
-console.log(ansArr.map((el) => el.join(" ")).join("\n"));
+console.log(result.length);
+console.log(result.map((el) => el.join(" ")).join("\n"));
 
 function hanoi(n, from, to, other) {
   if (n === 0) return;
   hanoi(n - 1, from, other, to);
-  ans++;
-  ansArr.push([from, to]);
+  result.push([from, to]);
   hanoi(n - 1, other, to, from);
 }
