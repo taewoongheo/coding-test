04b671f51c48a787867ae8bb9acc2719b84fd3e7
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-09 12:50:17 +0700

    [Gold IV] Title: 사분면, Time: 92 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Gold/1891. 사분면/사분면.js
+++ 백준/Gold/1891. 사분면/사분면.js
@@ -6,55 +6,49 @@ const [d, s, x, y] = require("fs")
   .map((el) => el.split(" "))
   .flat();
 
-let [sx, sy] = findPosition(0, Math.pow(2, d) - 1, 0, Math.pow(2, d) - 1, 0);
-sx += +x;
-sy -= +y;
-console.log(makePosition(0, Math.pow(2, d) - 1, 0, Math.pow(2, d) - 1, -1, ""));
+let fx = 0;
+let fy = 0;
+for (let i = s.length - 1; i >= 0; i--) {
+  const size = Math.pow(2, i);
 
-function findPosition(xs, xe, ys, ye, depth) {
-  if (depth === Number(d)) {
-    return [xs, ys];
-  }
-  const char = s.at(depth);
-  const xm = xs + Math.floor((xe - xs) / 2);
-  const ym = ys + Math.floor((ye - ys) / 2);
-  switch (char) {
-    case "1": {
-      return findPosition(xm + 1, xe, ys, ym, depth + 1);
-    }
-    case "2": {
-      return findPosition(xs, xm, ys, ym, depth + 1);
-    }
-    case "3": {
-      return findPosition(xs, xm, ym + 1, ye, depth + 1);
-    }
-    case "4": {
-      return findPosition(xm + 1, xe, ym + 1, ye, depth + 1);
-    }
-    default:
-      return [];
+  const char = s.at(d - i - 1);
+  if (char === "1") {
+    fx += size;
+  } else if (char === "2") {
+    continue;
+  } else if (char === "3") {
+    fy += size;
+  } else if (char === "4") {
+    fx += size;
+    fy += size;
   }
 }
 
-function makePosition(xs, xe, ys, ye, depth, str) {
-  if (depth === d - 1) {
-    return str;
-  }
-  const xm = xs + Math.floor((xe - xs) / 2);
-  const ym = ys + Math.floor((ye - ys) / 2);
+fx += +x;
+fy -= +y;
 
-  if (sx >= xm + 1 && sx <= xe && sy >= ys && sy <= ym) {
-    // 1사분면
-    return makePosition(xm + 1, xe, ys, ym, depth + 1, str + "1");
-  } else if (sx >= xs && sx <= xm && sy >= ys && sy <= ym) {
-    // 2사분면
-    return makePosition(xs, xm, ys, ym, depth + 1, str + "2");
-  } else if (sx >= xs && sx <= xm && sy >= ym + 1 && sy <= ye) {
-    // 3사분면
-    return makePosition(xs, xm, ym + 1, ye, depth + 1, str + "3");
-  } else if (sx >= xm + 1 && sx <= xe && sy >= ym + 1 && sy <= ye) {
-    // 4사분면
-    return makePosition(xm + 1, xe, ym + 1, ye, depth + 1, str + "4");
+let ans = "";
+for (let i = 0; i < d; i++) {
+  const limit = Math.pow(2, d - i);
+  const middle = Math.floor(limit / 2);
+
+  if (fx >= middle && fx < limit && fy >= 0 && fy < middle) {
+    ans += "1";
+    fx -= middle;
+  } else if (fx >= 0 && fx < middle && fy >= 0 && fy < middle) {
+    ans += "2";
+  } else if (fx >= 0 && fx < middle && fy >= middle && fy < limit) {
+    ans += "3";
+    fy -= middle;
+  } else if (fx >= middle && fx < limit && fy >= middle && fy < limit) {
+    ans += "4";
+    fx -= middle;
+    fy -= middle;
   }
-  return -1;
+}
+
+if (ans.length === Number(d)) {
+  console.log(ans);
+} else {
+  console.log(-1);
 }
