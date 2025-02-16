48b3a36509ba23150da9edd0e0bd54239c95883b
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-16 13:03:28 +0900

    [level 2] Title: 튜플, Time: 279.54 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -5,19 +5,14 @@ function solution(s) {
     let sarr = s.split('},').map((el) => el.split(',').map(Number));
     sarr = sarr.sort((a, b) => a.length - b.length);
     
-    const v = Array.from({length: 100001}, () => false);
     const answer = Array.from({length: sarr[sarr.length - 1].length}, () => 0);
 
     answer[0] = sarr[0][0];
-    v[sarr[0][0]] = true;
     let idx = 1;
     for (let i = 1; i < sarr.length; i++) {
         const size = sarr[i].length;
         for (let j = 0; j < size; j++) {
-            if (!v[sarr[i][j]]) {
-                v[sarr[i][j]] = true;
-                answer[idx++] = sarr[i][j];
-            }
+            if (!answer.includes(sarr[i][j])) answer[idx++] = sarr[i][j];
         }    
     }
     
