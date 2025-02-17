3b14dd1b3b4981270c42934f07c027a6ccd61874
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-17 13:50:51 +0900

    [level 3] Title: 정수 삼각형, Time: 28.43 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,29 @@
+// 문제요약: 삼각형의 꼭대기에서 대각선 방향으로 내려오는 경우 중 그 합이 가장 큰 경우
+// 알고리즘 선택: dp
+//  이전에 계산된 값을 다음 값이 가져와서 사용할 수 있다.
+//  2번째 줄에서 8을 선택할 경우, 세번째 줄의 8을 선택할 수 없다.
+//      만약 이때 세번째 줄의 숫자가 9999라면 답이 달라질 수 있다. 
+//          즉, 한 줄에서 나오는 최댓값 하나로만 판단하면 안됨.
+//  한 칸에는 두 개의 숫자가 영향을 미친다. 자신의 왼쪽, 오른쪽 위
+//  이 두 숫자 중 더 큰 값을 선택하면 된다. 
+//  이렇게 내려온 마지막 줄에서 가장 큰 값을 선택하면 된다.
+
+function solution(triangle) {
+    var answer = 0;
+    console.log(triangle);
+    
+    const dp = Array.from({length: triangle.length}, () => 0);
+    dp[0] = triangle[0][0];
+
+    for (let i = 0; i < triangle.length - 1; i++) {
+        const memo = [...dp];
+        for (let j = 0; j < i + 1; j++) {
+            dp[j] = Math.max(dp[j], memo[j] + triangle[i + 1][j]);
+            dp[j + 1] = memo[j] + triangle[i + 1][j + 1];
+            answer = Math.max(answer, dp[j]);
+            answer = Math.max(answer, dp[j + 1]);
+        }
+    }
+    
+    return answer;
+}
