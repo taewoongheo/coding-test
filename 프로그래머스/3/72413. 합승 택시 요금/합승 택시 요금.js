8ae1efa2a51e5930ccd2ed36a3f9c3823e389c71
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-21 15:06:39 +0900

    [level 3] Title: 합승 택시 요금, Time: 58.20 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,16 +1,15 @@
+// 문제요약: 두 정점으로 이동해야 하는 경우, 공통으로 이동하는 경우를 고려하여 최소 비용 구하기
+// 모든 정점들이 각 정점들로부터 최단 거리를 알고 있는 경우 -> 플로이드 워셜 알고리즘
+
 function solution(n, s, a, b, fares) {
     var answer = Infinity;
     
-    const g = Array.from({length: n + 1}, () => 
-        Array.from({length: n + 1}, () => Infinity)
-    )
-    
+    const g = Array.from({length: n + 1}, () => Array.from({length: n + 1}, () => Infinity));
     for (let i = 0; i < fares.length; i++) {
         const [s, e, c] = fares[i];
         g[s][e] = c;
         g[e][s] = c;
     }
-    
     for (let i = 1; i <= n; i++) {
         g[i][i] = 0;
     }
@@ -24,9 +23,7 @@ function solution(n, s, a, b, fares) {
             }
         }
     }
-    
-    console.log(g);
-    
+
     for (let i = 1; i <= n; i++) {
         answer = Math.min(answer, g[s][i] + g[i][a] + g[i][b]);
     }
