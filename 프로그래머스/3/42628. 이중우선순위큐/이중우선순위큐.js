cdab9626259d8a75a40af5ebdd73cbad3fcafb09
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-21 20:54:31 +0900

    [level 3] Title: 이중우선순위큐, Time: 45.54 ms, Memory: 49.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -0,0 +1,35 @@
+function solution(operations) {
+    var answer = [];
+    
+    const insert = (data) => {
+        if (answer.length === 0) {
+            answer.push(data);
+            return;
+        }
+        
+        let idx = 0;
+        while (idx < answer.length) {
+            if (answer[idx] < data) {
+                idx++;
+                continue;
+            }
+            answer.splice(idx, 0, data);
+            return;
+        }
+        answer.push(data);
+    }
+    
+    for (let i = 0; i < operations.length; i++) {
+        const [operation, num] = operations[i].split(' ');
+        if (operation === 'I') insert(Number(num));
+        else {
+            if (num === '1') answer.pop();
+            else answer.shift();
+        }
+    }
+    
+    if (answer.length === 0) {
+        return [0, 0];
+    }
+    return [answer[answer.length - 1], answer[0]];
+}
