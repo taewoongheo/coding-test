5beb347633baa177284974df9566c37c650b8459
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-21 14:41:05 +0900

    [level 3] Title: 정수 삼각형, Time: 21.38 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,19 +1,17 @@
 function solution(triangle) {
-    var answer = 0;
-    
-    const dp = Array.from({length: triangle.length}, () => 0);
-    dp[0] = triangle[0][0];
+
+    const dp = Array.from({length: triangle.length}, () => Array.from({length: triangle.length}, () => 0));
+    dp[0][0] = triangle[0][0];
     
     for (let i = 0; i < triangle.length - 1; i++) {
-        for (let j = i; j >= 0; j--) {
-            dp[j + 1] = Math.max(dp[j] + triangle[i + 1][j + 1], dp[j + 1]);
-            dp[j] += triangle[i + 1][j];
-            if (i === triangle.length - 2) {
-                answer = Math.max(answer, dp[j + 1]);
-                answer = Math.max(answer, dp[j]);
-            }
+        const next = triangle[i + 1];
+        for (let j = 0; j < triangle[i].length; j++) {
+            const num = dp[i][j];
+            dp[i + 1][j] = Math.max(num + next[j], dp[i + 1][j]);
+            dp[i + 1][j + 1] = num + next[j + 1];
         }
     }
     
-    return answer;
+    
+    return Math.max(...dp[triangle.length - 1]);
 }
