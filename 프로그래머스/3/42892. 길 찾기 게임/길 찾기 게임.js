f3eb9ed8fe36cbef993e0b35d53a4ad15643749b
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-22 13:02:15 +0900

    [level 3] Title: 길 찾기 게임, Time: 205.71 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.js
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.js
@@ -1,13 +1,6 @@
-// 문제요약: 주어진 노드들의 위치를 바탕으로 트리를 만들고, 전위순회, 후위순회 결과를 리턴
-// 알고리즘 선택: 
-//  트리의 y가 높을수록 루트에 가까움
-//  y순으로 정렬 후, 루트부터 결정
-//  이후, x값을 기준으로 왼쪽 자식 또는 오른쪽 자식을 선택하여 트리를 완성하면 된다. 
-//      부모보다 x값이 작다면 왼쪽자식, x값이 크다면 오른쪽 자식
-
 class Node {
-    constructor(id, x, y) {
-        this.id = id;
+    constructor(num, x, y) {
+        this.num = num;
         this.x = x;
         this.y = y;
         this.left = null;
@@ -16,47 +9,41 @@ class Node {
 }
 
 function solution(nodeinfo) {
-    var answer = [[]];
-    
-    const nodes = nodeinfo.map(([x, y], idx) => [x, y, idx + 1]);
-    nodes.sort(([, y1], [, y2]) => y2 - y1);
 
-    const root = nodes.shift();
-    const rootNode = new Node(root[2], root[0], root[1]);
-    for (let i = 0; i < nodes.length; i++) {
-        const node = nodes[i];
-        makeTree(rootNode, new Node(node[2], node[0], node[1]));
-    }
+    const nodes = nodeinfo.map((v, i) => [i + 1, ...v]);
+    nodes.sort(([num1, x1, y1], [num2, x2, y2]) => y2 - y1);
     
-    console.log(rootNode);
-    
-    const preorder = [];
-    const postorder = [];
-    
-    getPreorder(rootNode);
-    getPostorder(rootNode);
-
-    function makeTree(parent, child) {
-        const side = child.x < parent.x ? 'left' : 'right';
+    const makeTree = (parent, num, x, y) => {
+        const side = parent.x > x ? 'left' : 'right';
         
-        if (!parent[side]) {
-            parent[side] = child;
+        if (parent[side]) {
+            makeTree(parent[side], num, x, y);
         } else {
-            makeTree(parent[side], child);
+            parent[side] = new Node(num, x, y);
         }
     }
     
-    function getPreorder(node) {
-        preorder.push(node.id);
-        if (node.left) getPreorder(node.left);
-        if (node.right) getPreorder(node.right);
+    const root = new Node(...nodes.shift());
+    for (let i = 0; i < nodes.length; i++) {
+        makeTree(root, nodes[i][0], nodes[i][1], nodes[i][2]);
     }
     
-    function getPostorder(node) {
-        if (node.left) getPostorder(node.left);
-        if (node.right) getPostorder(node.right);
-        postorder.push(node.id);
+    const pre = [];
+    const post = [];
+    
+    const preorder = (root) => {
+        pre.push(root.num);
+        if (root.left) preorder(root.left);
+        if (root.right) preorder(root.right);
+    }
+    const postorder = (root) => {
+        if (root.left) postorder(root.left);
+        if (root.right) postorder(root.right);
+        post.push(root.num);
     }
     
-    return [preorder, postorder];
+    preorder(root);
+    postorder(root);
+    
+    return [pre, post];
 }
