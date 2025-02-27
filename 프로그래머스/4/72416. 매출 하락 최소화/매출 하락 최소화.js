bec6cc6ffba59b8627d14e4456894c810c009264
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-02-27 14:30:48 +0900

    [level 4] Title: 매출 하락 최소화, Time: 437.90 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.js
+++ 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.js
@@ -1,71 +1,68 @@
-// 문제요약: 각 팀에서 한 명 이상 선택, 선택된 사람들의 매출액이 최소화된 경우의 매출액 구하기
+// 문제요약: 각 팀마다 한 명씩 뽑을 때, 최소가 되는 매출액 구하기
 // 입력: 
-//  sales: 인덱스 기준 직원들의 매출액: 2<=sales<=300000
-//  links: [팀장-팀원] 관계: links<=sales.length - 1
-//      [팀장-팀원] = [a, b]: 서로 다른 자연수, a,b <= sales.length - 1
-//          ceo는 항상 직원번호 1번이므로 b != 1
-// 알고리즘 선택: dp
-//  각 팀마다 팀장을 포함시키면, 나머지 팀원들은 포함시키던 말던 상관 x
-//  각 팀마다 팀장을 포함시키지 않으면, 나머지 팀원들 중 한명은 무조건 포함시켜야 함
-//  dp[1][1] = 1번 팀장을 포함시킨 경우
-//      = sales[1] + min(dp[9][0], dp[9][1]) + min(dp[5][0], dp[5][1]) + min(dp[3][0], dp[3][1]);
-//  dp[1][0] = 1번 팀장을 포함시키지 않은 경우
+//  salse: 인덱스 기준 직원들의 매출액: 2<=sales<=300000
+//  links: [팀장-팀원]: links<=sales.length-1
+// 알고리즘 선택: 
+//  팀마다 한명씩 최소가 되도록 골라야 한다
+//  만약 팀장이 팀원에도 속해있다면, 두 팀에 동시에 속해있기 때문에 한번에 조건을 만족할 수 있다
+//      하지만 팀장=10000, 팀원1=1, 팀원2=1 이라면, 팀원 두 명을 선택하는게 더 유리하다
+//  따라서 한 팀에 팀장이 포함된 경우와 포함되지 않았을 경우로 나눠 고려한다
+//  D에서부터 팀원을 고른다고 가정해보자
+//  팀장을 포함하거나, 포함하지 않을 경우 매출액은 최적해다. 또한 이를 재사용하여 팀 C에서 사용해야 하기 때문에 dp
+//  dp[1][1]: 팀장을 포함하는 경우 => 조건을 바로 만족하므로, 팀원 포함여부는 상관없다
+//  dp[1][0]: 팀장을 포함하지 않으면, 팀원 중 한명이라도 반드시 포함해야 한다
 //      = dp[9][1] + min(dp[5][0], dp[5][1]) + min(dp[3][0], dp[3][1])
-//      = min(dp[9][0], dp[9][1]) + dp[5][1] +min(dp[3][0], dp[3][1])
+//      = min(dp[9][0], dp[9][1]) + dp[5][1] + min(dp[3][0], dp[3][1])
 //      = min(dp[9][0], dp[9][1]) + min(dp[5][0], dp[5][1]) + dp[3][1]
-//  bottom up
-// 부분문제분해:
-//  팀장들을 저장
-//      {} 객체에 담아 인덱스로 접근하도록. { num, employees }
-//  A팀부터 팀원들을 검사, 만약 팀원들 중 팀장이 있다면 재귀
-//      팀장의 팀원들을 검사, 만약 팀원들 중 팀장이 없다면
-//          dp[팀장][0] = 팀장이 포함되지 않은 경우
-//          dp[팀장][1] = 팀장이 포함된 경우
-//  시간복잡도: 노드별로 한번씩만 탐색하면 되기 때문에 O(n)
 
 function solution(sales, links) {
-    var answer = 0;
-    
-    const tree = {};
-    const dp = Array.from({length: sales.length + 1}, () => [0, 0]);
-    for (let i = 0; i < sales.length; i++) {
-        tree[i + 1] = { num: sales[i], employees: [] };
-        dp[i + 1][1] = sales[i];
-    }
-    
-    for (let i = 0; i < links.length; i++) {
-        const [p, c] = links[i];
-        tree[p].employees.push(c);
-    }
-    
 
+    const dp = Array.from({length: sales.length + 1}, () => [0, 0]);
+    let tree = sales.reduce((tree, el, i) => {
+        dp[i + 1][1] = el;
+        tree[i + 1] = {
+            num: el,
+            emp: []
+        };
+        return tree;
+    }, {});
+    
+    tree = links.reduce((tree, link) => {
+        const [s, e] = link;
+        tree[s].emp.push(e);
+        return tree;
+    }, tree);
+    
+    console.log(dp);
     const calDP = (node) => {
         
-        for (const employee of tree[node].employees) {
-            if (tree[employee].employees.length > 0) calDP(employee);
+        for (const em of tree[node].emp) {
+            if (tree[em].emp.length > 0) calDP(em);
         }
-    
-        let checkGroup = false;
-        dp[node][0] = tree[node].employees.reduce((acc, employee) => {
-            if (dp[employee][0] < dp[employee][1]) return acc + dp[employee][0];
+        
+        let grouped = false;
+        dp[node][0] = tree[node].emp.reduce((acc, em) => {
+            if (dp[em][0] < dp[em][1]) return acc += dp[em][0];
             else {
-                checkGroup = true;
-                return acc + dp[employee][1];
+                grouped = true;
+                return acc += dp[em][1];
             }
         }, 0);
         
-        dp[node][1] = dp[node][0] + tree[node].num;
+        dp[node][1] = dp[node][0] + tree[node].num
         
-        if (!checkGroup) {
+        if (!grouped) {
             let v = Infinity;
-            for (const employee of tree[node].employees) {
-                v = Math.min(dp[employee][1] - dp[employee][0], v);
+            for (const em of tree[node].emp) {
+                const diff = dp[em][1] - dp[em][0];
+                v = Math.min(v, diff);
             }
             dp[node][0] += v;
         }
     }
     
     calDP(1);
+    console.log(dp);
     
     return Math.min(dp[1][0], dp[1][1]);
 }
