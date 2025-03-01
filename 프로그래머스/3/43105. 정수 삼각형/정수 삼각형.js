1d9e0463cd0d833d49fde2d81145d2249fec99ce
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-01 17:24:33 +0900

    [level 3] Title: 정수 삼각형, Time: 4.24 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,24 +1,18 @@
-// 문제요약: 삼각형의 꼭대기에서 아래로 내려갈 때 가장 큰 숫자 구하기
-// 입력: 
-//  triangle: 삼각형 정보 2차원 배열, 1<=triangle.length<=500
-// 알고리즘 선택: 
-//  가장 위에서부터 dp로 내려오면 된다
-
+// dp[i + 1][j] = Math.max(dp[i][j] + triangle[i + 1][j], dp[i][j]);
+// dp[i + 1][j + 1] = Math.max(dp[i][j] + triangle[i + 1][j + 1], dp[i][j + 1]);
 
 function solution(triangle) {
     var answer = 0;
     
-    const dp = Array.from({length: triangle.length}, () => 
-                         Array.from({length: triangle.length}, () => 0));
-    dp[0][0] = triangle[0][0];
+    const dp = Array.from({length: triangle.length}, () => 0);
+    dp[0] = triangle[0][0];
     
     for (let i = 0; i < triangle.length - 1; i++) {
-        for (let j = 0; j <= i; j++) {
-            const item = dp[i][j];
-            dp[i + 1][j] = Math.max(dp[i + 1][j], triangle[i + 1][j] + item);
-            dp[i + 1][j + 1] = triangle[i + 1][j + 1] + item;
+        for (let j = triangle[i].length - 1; j >= 0; j--) {
+            dp[j + 1] = Math.max(dp[j] + triangle[i + 1][j + 1], dp[j + 1]);
+            dp[j] += triangle[i + 1][j];
         }
     }
     
-    return Math.max(...dp[triangle.length - 1]);
+    return Math.max(...dp);
 }
