63db348c9df538228ad2e2b30d3ee50968e44ffa
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-02 10:31:53 +0900

    [level 2] Title: 완전범죄, Time: 2.09 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,8 +1,16 @@
-// dp[p][b] = B의 흔적이 b개일 때, 물품p까지 고려했을 때 A의 흔적
-// A를 선택하는 경우
-//  dp[p][b] = Math.min(dp[p][b], dp[p - 1][b] + a);
-// B를 선택하는 경우
-//  dp[p][b + b흔적] = Math.min(dp[p][b+b흔적], dp[p-1][b] + b흔적)
+// 문제요약: A도둑이 남긴 흔적의 누적값이 최소가 되는 값을 구하기
+// 알고리즘 선택: 
+//  모든 물품을 B가 갖고 있다고 했을 때, 그 중 A가 물품을 골라 최솟값을 구하는 방식
+//      dfs-물건의 개수는 40개이므로 O(2^40) 시간초과
+//  물품들은 어쨌든 모두 골라야 한다. 즉, A B 중 누군가는 골라야 한다
+//  우리가 구하고자 하는 값은 A 흔적의 최솟값이다. 그리고 물품을 고르던지 말던지 결정해야 함
+//  물품선택 시 각 시행이 독립적
+//  따라서 dp로 풀 수 있다
+//      dp[고려한 물품][B의 흔적개수] = A흔적개수
+//          A가 선택할 경우
+//              dp[물품][B 흔적개수] = Math.min(dp[물품 - 1][B흔적개수] + 물품[0])
+//          B가 선택할 경우
+//              dp[물품][B 흔적개수 + 물품[1]] = Math.min(dp[물품 - 1][B 흔적개수 + 물품[1]])
 
 function solution(info, n, m) {
     var answer = Infinity;
@@ -11,21 +19,22 @@ function solution(info, n, m) {
                          Array.from({length: m}, () => Infinity));
     dp[0][0] = 0;
     
-    for (let p = 1; p <= info.length; p++) {
-        const [a, b] = info[p - 1];
-        for (let c = 0; c < m; c++) {
-            // A를 선택
-            dp[p][c] = Math.min(dp[p - 1][c] + a, dp[p][c]);
+    for (let i = 1; i <= info.length; i++) {
+        const [a, b] = info[i - 1];
+        for (let j = 0; j < m; j++) {
+            // A가 선택
+            dp[i][j] = Math.min(dp[i - 1][j] + a, dp[i][j]);
             
-            // B를 선택
-            if (c + b >= m) continue;
-            dp[p][c + b] = Math.min(dp[p - 1][c], dp[p][c + b]);
+            // B가 선택
+            if (j + b >= m) continue;
+            dp[i][j + b] = Math.min(dp[i - 1][j], dp[i][j + b]);
         }
     }
     
-    answer = dp[info.length].reduce((acc, el) => {
-        return el < n ? Math.min(acc, el) : acc;
-    }, Infinity);
+    answer = dp[info.length].reduce((answer, cur) => {
+        if (cur < n) return Math.min(answer, cur);
+        return answer;
+    }, answer);
     
     return answer === Infinity ? -1 : answer;
 }
