8878178e10d9b73d6418a9d5f9a3f93173d7853f
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-02 10:53:07 +0900

    [level 3] Title: 봉인된 주문, Time: 512.95 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
@@ -6,21 +6,24 @@ function solution(n, bans) {
         for (let i = 0; i < str.length; i++) {
             num = num * 26 + (str.charCodeAt(i) - 96);
         }
+        
         return num;
     }
     
+    // 메서드가 생각이 안남. 
+    const al = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     const numToStr = (num) => {
         let str = '';
         while (num > 0) {
             num--;
-            str = String.fromCharCode(97 + (num % 26)) + str;
+            str = al[num % 26] + str;
             num = Math.floor(num / 26);
         }
+        
         return str;
     }
     
-    bans.sort((a, b) => strToNum(a) - strToNum(b));
-    
+    bans = bans.sort((a, b) => strToNum(a) - strToNum(b));
     for (const ban of bans) {
         if (n >= strToNum(ban)) n++;
     }
