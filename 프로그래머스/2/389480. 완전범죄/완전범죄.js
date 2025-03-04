64137293d0cabe73d598df92ff83455a7a7a9b61
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-04 10:26:53 +0900

    [level 2] Title: 완전범죄, Time: 1.15 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,19 +1,19 @@
-// 문제요약: A도둑이 남긴 흔적의 누적값이 최소가 되는 값을 구하기
+// 문제요약: 모든 물건을 훔쳤을 때, A도둑이 남긴 흔적의 누적 개수의 최솟값을 구하는 문제
 // 알고리즘 선택: 
-//  모든 물품을 B가 갖고 있다고 했을 때, 그 중 A가 물품을 골라 최솟값을 구하는 방식
-//      dfs-물건의 개수는 40개이므로 O(2^40) 시간초과
-//  물품들은 어쨌든 모두 골라야 한다. 즉, A B 중 누군가는 골라야 한다
-//  우리가 구하고자 하는 값은 A 흔적의 최솟값이다. 그리고 물품을 고르던지 말던지 결정해야 함
-//  물품선택 시 각 시행이 독립적
-//  따라서 dp로 풀 수 있다
-//      dp[고려한 물품][B의 흔적개수] = A흔적개수
-//          A가 선택할 경우
-//              dp[물품][B 흔적개수] = Math.min(dp[물품 - 1][B흔적개수] + 물품[0])
-//          B가 선택할 경우
-//              dp[물품][B 흔적개수 + 물품[1]] = Math.min(dp[물품 - 1][B 흔적개수 + 물품[1]])
+//  최솟값을 구해야 됨
+//  각 물건에 대해 누가 선택할 지 결정, 그 선택이 서로 독립적
+//  B도둑이 n번째에 특정 물건을 선택할 지 말지 결정할 때, 그 상태까지 오는 것에 중복이 발생
+//  점화식: 
+//      dp[물건][B의 흔적] = A의 흔적
+//      두 가지 경우가 존재, 이전물건에서
+//          A가 선택하는 경우
+//              dp[물건+1][B흔적] = A흔적 + 물건흔적
+//          B가 선택하는 경우
+//              dp[물건+1][B흔적+물건흔적] = A흔적
+//  O(n^2)
 
 function solution(info, n, m) {
-    var answer = Infinity;
+    var answer = 0;
     
     const dp = Array.from({length: info.length + 1}, () => 
                          Array.from({length: m}, () => Infinity));
@@ -22,19 +22,17 @@ function solution(info, n, m) {
     for (let i = 1; i <= info.length; i++) {
         const [a, b] = info[i - 1];
         for (let j = 0; j < m; j++) {
-            // A가 선택
             dp[i][j] = Math.min(dp[i - 1][j] + a, dp[i][j]);
             
-            // B가 선택
             if (j + b >= m) continue;
-            dp[i][j + b] = Math.min(dp[i - 1][j], dp[i][j + b]);
+            dp[i][j + b] = dp[i - 1][j];
         }
     }
     
-    answer = dp[info.length].reduce((answer, cur) => {
-        if (cur < n) return Math.min(answer, cur);
+    answer = dp[dp.length - 1].reduce((answer, el) => {
+        if (el < n) return Math.min(answer, el);
         return answer;
-    }, answer);
+    }, Infinity);
     
     return answer === Infinity ? -1 : answer;
 }
