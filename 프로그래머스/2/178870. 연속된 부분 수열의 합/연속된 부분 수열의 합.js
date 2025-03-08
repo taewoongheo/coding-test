fbc28a562ac1702f69e83ebc0e8e5aca3f56e061
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-08 20:25:27 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 215.38 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -2,21 +2,18 @@ function solution(sequence, k) {
     let results = [];
     let rIdx = 0; 
     let lIdx = 0; 
-    let sum = sequence[0];
+    let sum = 0;
     
     while (rIdx < sequence.length) {
         if (sum < k) {
-            sum += sequence[++rIdx];
+            sum += sequence[rIdx++];
         } else if (sum > k) {
             sum -= sequence[lIdx++];
         } else {
             results.push([lIdx, rIdx]);
-            sum += sequence[++rIdx];
+            sum += sequence[rIdx++];
         }
     }
     
-    return results.sort((a, b) => {
-        if ((a[1]-a[0]) === b[1]-b[0]) return a[0] - b[0];
-        return (a[1]-a[0]) - (b[1]-b[0]);
-    })[0];
+    console.log(results)
 }
