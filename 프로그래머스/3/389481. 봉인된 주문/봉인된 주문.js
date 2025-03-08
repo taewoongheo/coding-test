b5cbffb6d1fdb3bf62d5ec28430ab3966b2a778c
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-08 13:11:09 +0900

    [level 3] Title: 봉인된 주문, Time: 553.39 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
@@ -1,8 +1,16 @@
+// 문제요약: 삭제된 주문들을 고려할 때 n 번째 주문을 return
+// 알고리즘 선택: 
+//  문자열이 나오는 순서가 길이, 알파벳 순서
+//  따라서 문자열<->숫자 변환이 가능
+//  문자열을 숫자로 변환하여 비교
+//  bans를 내림차순으로 정렬
+//  하나씩 pop하면서 n과 비교. 이때 n보다 작으면 n++
+
 function solution(n, bans) {
     var answer = '';
     
     const strToNum = (str) => {
-        let num = 0; 
+        let num = 0;
         
         for (let i = 0; i < str.length; i++) {
             num = num * 26 + (str.charCodeAt(i) - 96);
@@ -24,10 +32,8 @@ function solution(n, bans) {
     }
     
     bans = bans.sort((a, b) => strToNum(b) - strToNum(a));
-    
     while (bans.length) {
         if (n >= strToNum(bans.pop())) n++;
-        else break;
     }
     
     return numToStr(n);
