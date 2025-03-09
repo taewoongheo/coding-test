b81f85d3d15cfb73f3c35d09023f94961de5c628
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-09 09:51:21 +0900

    [level 2] Title: 완전범죄, Time: 1.19 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,48 +1,51 @@
-// 문제요약: 모든 물건을 훔쳤을 때, A 흔적의 최솟값
+// 문제요약: A,B 도둑이 모든 물건을 훔칠 때, A도둑의 흔적이 최솟값 구하기
 // 알고리즘 선택: 
-//  모든 물건을 훔치려면 일단 B가 모두 훔친 상태에서 A에게 하나씩 주는 방식?
-//  이 경우 1번물건부터 40번물건까지 하나씩 줘야 함
-//  이때 중복이 발생. 예를 들어 1, 2, 3, 7, 14 번 물건을 줬다고 해보자.
-//      그럼 조합으로 구한다고 해도, 2,3,7,14 번 물건의 값을 중복해서 구해야 함
-//  어쨌든 모든 물건을 훔쳐야 함. i번째 물건을 누가 훔칠 지 결정하면 됨
-//  또한 각 선택이 독립적임
-//  =>dp
-//  점화식 구하기
-//      현재 상태: 현재물건,b의 흔적, a의 흔적
-//      구해야 하는 최솟값은 a의 흔적이므로, 이걸 값으로 둠
-//      dp[현재 상태][b의 흔적] = a의 흔적
-//          두 가지 경우가 존재, 현재 i번째 물건이라고 했을 때
-//              a가 선택하는 경우
-//                  dp[i + 1][b] = min(dp[i + 1][b] + info[i + 1][a], )
-//              b가 선택하는 경우
-//                  dp[i + 1][b + b흔적] = dp[i][b] => 갱신하는 순서가 왼쪽부터 이므로 이 값은 비교할 필요 없이 바로 채워 넣으면 됨
+//  일단 모든 물건을 훔치려면 B가 모든 물건을 갖고 있는 상태에서 A,B 조건이 만족하도록 A의 개수를 조정하면 됨
+//  B가 모든 물건을 훔친 상태에서 A의 조합을 구해 그 수만큼 빼고 값을 갱신하는 방법
+//      하나의 물건을 A가 선택하거나, 선택하지 않을 수 있으므로 O(40!)의 시간복잡도를 가짐
+//      이때 i 번째 물건을 고를지 말지 선택할 때, 이전에 고른 물건의 상태가 중복됨
+//  i번째 물건을 선택할 지 말지 고르는 것은 결정문제임
+//  또한 i번째 물건을 선택할 때 이전까지의 물건의 선택이 중복됨
+//  물건을 선택하는 각 상태가 부분문제의 최적해가 될 수 있음(A의 최솟값이 최적해)
+//  점화식 세우기
+//      상태: 현재 고를 물건, A의 흔적, B의 흔적
+//      구해야 하는 최솟값은 A의 흔적, A의 작을 수록 최적해
+//      dp[현재고를물건][B의흔적]=A의흔적
+//      두 가지 선택이 가능
+//          현재 물건을 A가 선택
+//              dp[i][b흔적]=min(dp[i-1][b흔적]+a흔적, )
+//          현재 물건을 B가 선택
+//              dp[i][b흔적+현재물건의 b흔적]=dp[i-1][b흔적]
+//                  방향자체가 왼쪽에서 오른쪽으로 정해져있기 때문에 최솟값 갱신 필요없이 바로 값으로 넣으면 됨
 // 부분문제 분해: 
-//  dp[info.length][m]
-//  for i in info.length - 1:
-//      dp~
+//  dp[물건개수+1][m]
+//  dp[0][0]=0 물건을 가지지 않은 상태로 초기화
+//  for 1~i in i:
+//      a, b = info[i - 1]
+//      for 0~m in j:
+//          dp값 갱신
 
 function solution(info, n, m) {
-    var answer = Infinity;
+    var answer = 0;
     
     const dp = Array.from({length: info.length + 1}, () => 
                          Array.from({length: m}, () => Infinity));
     dp[0][0] = 0;
     
-    for (let i = 0; i < info.length; i++) {
-        const [a, b] = info[i];
-        for (let j = 0; j < m; j++) {
-            if (dp[i][j] === Infinity) continue;
-            
-            dp[i + 1][j] = Math.min(dp[i][j] + a, dp[i + 1][j]);
+    for (let i = 1; i <= info.length; i++) {
+        const [a, b] = info[i - 1];
+        for (let j = 0; j < m ; j++) {
+            dp[i][j] = Math.min(dp[i - 1][j] + a, dp[i][j]);
             
             if (j + b >= m) continue;
-            dp[i + 1][j + b] = dp[i][j];
+            dp[i][j + b] = dp[i - 1][j];
         }
     }
     
-    for (let i = 0; i < dp[info.length].length; i++) {
-        if (dp[info.length][i] < n) answer = Math.min(dp[info.length][i], answer);
-    }
+    answer = dp[dp.length - 1].reduce((answer, cur) => {
+        if (cur < n) return Math.min(answer, cur);
+        return answer;
+    }, Infinity);
     
     return answer === Infinity ? -1 : answer;
 }
