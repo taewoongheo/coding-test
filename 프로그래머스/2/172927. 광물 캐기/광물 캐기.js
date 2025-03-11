f1aa420dce84a3b658a43f631de84f93acf0e9f0
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-11 10:33:47 +0900

    [level 2] Title: 광물 캐기, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,100 +1,75 @@
-// 문제요약: 광물을 캘 때 최소한의 피로도 구하기
+// 문제요약: 작업을 끝내기까지 최소한의 피로도 구하기
 // 알고리즘 선택: 
-//  곡괭이 하나당 최대 5번 사용가능
-//  한 번 사용한 곡괭이는 끝까지 사용해야 함
-//  광물을 모두 캐거나, 곡괭이를 사용할 수 없을 때까지
-//  특정 광물에서 어떤 곡괭이를 사용할 것인가?
-//      한번 사용하기 시작한 곡괭이는 최대 5번 사용가능
-//  곡괭이는 최대 15개. 순열을 만든다고 하면 15^15(각 자릿수마다 15개씩 가능)
-//      곡괭이의 순서를 직접 모두 고려하면 시간초과
-//  특정 상태에서 행동할 수 있는 건 두 가지. 현재 곡괭이를 계속 사용하거나, 다른 곡괭이를 사용하거나
-//  그리고 그 상태까지 경우의 수가 계속 중복(n+1 상태에서보면, n까지의 상태가 중복됨)
-//  근데 곡괭이 하나 당 5번 이하로 쓸 수 있는 것 때문에 dp가 안됨
-//      고려해야 하는 상태가 곡괭이, 곡괭이 사용횟수, 현재 광물, 피로도 4개임
-//      일단 dp는 아니다 그럼
-//      그리고 그렇게 부분문제를 잡았다하더라도, 그게 최적해가 아님
-//          왜냐하면 현재 피로도가 낮게 하려고 다이아 곡괭이 썼다가 나중에 필요 시 못쓸 수 있음
-//              => 근데 이렇게 하는거 맞나? 이건 greedy 아닌가
-//  dfs?
-//      구현은 가능한데, 시간복잡도를 계산해봐야 함
-//      현재 곡괭이 당 5개의 경우의 수가 생김
-//      또한 남은 곡괭이에서 하나를 골라야 됨
-//      picks는 최대 15개, 그리고 곡괭이 하나 당 5. 
-//          시간복잡도 어떻게 잡음? 
-//              현재 곡괭이에서 15개 중 하나를 고르거나, 아님 5번 이하로 사용=>15!x5
-//          하지만 minerals의 깊이는 최대 50임. 
+//  작업을 끝내다=
+//      모든 곡괭이를 사용하거나, 모든 광물을 캠
+//  최적화문제이므로 dp의심
+//  고려해야 하는 상태: 사용중인 곡괭이, 광물, 피로도
+//      dp[광물][곡괭이]=피로도: i번째 광물을 j곡괭이로 캘 때 피로도
+//      피로도를 작은 값으로 선택=>부분문제의 최적해 가능
+//      그럼 각 부분문제들이 독립적인가?=특정 상태에 여러 경로로 도달가능한가?
+//          사용할 수 있는 곡괭이의 수가 정해져 있음. 
+//              만약 현재 피로도를 위해 다이아 곡괭이를 사용하면 나중에 사용하지 못함. 
+//                  즉, 특정 상태까지 도달하려면 제한된 경로를 따라가야됨(i번째에서 피로도가 높아도 곡괭이를 사용 x-나중에 써야 하기 때문에)
+//                  => dp 아님
+//  greedy 의심
+//      곡괭이로 반드시 5번 연속으로 캐야 함
+//      즉, 광물은 5개씩 나뉨
+//      나뉜 광물의 비용을 계산하고, 정렬 후, 좋은 곡괭이를 하나씩 사용하면 됨
+// 부분문제 분해: 
+//  5개씩 그룹화, 이때 다이아, 철, 돌의 개수를 카운트
+//  그룹을 정렬, 다이아 > 철 > 돌 순서대로
+//  좋은 곡괭이부터 사용해서 첫 번째 그룹부터 계산
 
 function solution(picks, minerals) {
-    var answer = Infinity;
+    var answer = 0;
     
-    const status = [];
-    let idx = 0;
+    const len = Math.ceil(minerals.length / 5);
+    const maxLen = picks[0] + picks[1] + picks[2];
+    let marr = [];
     
-    for (let i = 0; i < picks.length; i++) {
-        let r = 'diamond'; 
-        if (i === 1) r = 'iron'; 
-        else if (i === 2) r = 'stone';
+    for (let i = 0; i < len; i++) {
+        if (i >= maxLen) break;
         
-        for (let j = 0; j < picks[i]; j++) {
-            status[idx++] = {
-                r: r,
-                left: 5,
-                used: false,
+        const arr = [0, 0, 0];
+        
+        minerals.splice(0, 5).forEach(item => {
+            switch(item) {
+                case "diamond" : arr[0]++; break;
+                case "iron" : arr[1]++; break;
+                default : arr[2]++; break;
             }
-        }
+        });
+        
+        marr.push(arr)
     }
     
-    const tlqkf = [];
-    dfs(null, status, 0, 0);
-    console.log(tlqkf)
+    marr = marr.sort((a, b) => {
+        if (a[0] !== b[0]) return b[0] - a[0];
+        else if (a[1] !== b[1]) return b[1] - a[1];
+        return b[0] - a[0];
+    });
     
+    const tired = [
+        [1, 1, 1], 
+        [5, 1, 1],
+        [25, 5, 1],
+    ]
     
-    function dfs(cur, v, cnt, idx) {
+    for (let m = 0; m < marr.length; m++) {
+        const [d, i, s] = marr[m];
         
-        if (idx === minerals.length - 1) {
-            answer = Math.min(answer, cnt);
-            return;
-        }
+        let idx = -1; 
+        if (picks[0] > 0) idx = 0;
+        else if (picks[1] > 0) idx = 1;
+        else if (picks[2] > 0) idx = 2;
         
-        const m = minerals[idx];
+        if (idx === -1) break;
         
-        if (cur !== null && cur.left > 0) {
-            const f = getFatigue(cur.r, m);
-            v.left--;
-            dfs(cur, v, cnt + f, idx + 1);
-            v.left++;
-        }
-        
-        for (let i = 0; i < v.length; i++) {
-            if (!v[i].used) {
-                const f = getFatigue(v[i].r, m);
-                
-                v[i].used = true;
-                v[i].left--;
-                dfs(v[i], v, cnt + f, idx + 1);
-                v[i].used = false;
-                v[i].left++;
-            }
-        }
-    }
-    
-    function getFatigue(pick, mine) {
-        if (pick === 'diamond') {
-            return 1;
-        } else if (pick === 'iron') {
-            if (mine === 'diamond') {
-                return 5;
-            }
-            return 1;
-        } else {
-            if (mine === 'diamond') {
-                return 25; 
-            } else if (mine === 'iron') {
-                return 5;
-            }
-            return 1;
-        }
-    } 
+        picks[idx]--;
+        answer += tired[idx][0] * d;
+        answer += tired[idx][1] * i;
+        answer += tired[idx][2] * s;
+    };
     
     return answer;
 }
