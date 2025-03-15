f50c9df93043fcbeeef1d2bff94a78ccdb07efcd
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-15 10:23:01 +0900

    [level 2] Title: 광물 캐기, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,31 +1,21 @@
-// 문제요약: 모든 광물들을 캘 때 최소한의 피로도 구하기
+// 문제요약: 광물을 모두 캐거나 곡괭이를 모두 사용할 때까지 최소 피로도
 // 알고리즘 선택: 
-//  곡괭이로 5번 연속으로 캐야함
-//  상태: 현재광물, 곡괭이, 피로도
-//      dp[현재광물][곡괭이]=피로도: 광물i이고 j곡괭이일 때 최소 피로도
-//      부분문제의 최적해?
-//          낮은 피로도를 유지하기 위해선 가장 좋은 곡괭이부터 사용해야 함
-//          이 경우 나중에 가서 다이아가 연속으로 나왔을 때 불리해질 수 있음
-//          즉, 부분문제의 해는 최적해가 아님
-//  greedy
-//      광물은 무조건 5개씩 캐야됨
-//      광물을 5개씩 세트로 만들고, 세트를 가중치 순서대로 정렬
-//      이후 좋은 곡괭이부터 사용하면 됨
-// 부분문제 분해: 
-//  만약 곡괭이로 캘 수 있는 광물 수가 더 많다면, 광물의 최대 수 만큼. 광물이 더 많다면, 곡괭이 최대 수만큼까지만 그룹화
-//  가중치 기준 오름차순 정렬
-//  다이아->철->돌 곡괭이 순으로 사용
+//  한번 선택한 곡괭이로 5개를 무조건 캐야함
+//  광물을 캐는 순서가 정해져 있음
+//      즉, 광물은 5개씩 그룹화 뒤, 그 순서대로만 캐야함
+//  만약 5개씩 그룹화된 광물을 좋은 곡괭이부터 사용한다면 나중에 피로도가 높아질 수 있음
+//  따라서 광물을 피로도 순서대로 정렬한 뒤, 좋은 곡괭이부터 사용하면 됨
+//  어차피 정렬해도 5개씩만 캐면 된다는 순서만 유지하면 되기 때문에 상관없음
 
 function solution(picks, minerals) {
     var answer = 0;
     
     const max = Math.min((picks[0] + picks[1] + picks[2]) * 5, minerals.length);
-    let marr = [0, 0, 0];
     let arr = [];
+    let marr = [0, 0, 0];
     let cnt = 0;
     
-    for (let i = 0; i < minerals.length; i++) {
-        if (max <= i) break;
+    for (let i = 0; i < max; i++) {
         const m = minerals[i];
         
         if (m === 'diamond') marr[0]++;
@@ -48,24 +38,25 @@ function solution(picks, minerals) {
     });
     
     const tired = [
-        [1, 1, 1], 
+        [1, 1, 1],
         [5, 1, 1],
         [25, 5, 1],
     ]
-    arr.forEach(item => {
-        const [d, i, s] = item;
+    
+    arr.forEach(el => {
+        const [d, i, s] = el;
         
         let idx = 0;
         if (picks[0] > 0) idx = 0;
         else if (picks[1] > 0) idx = 1;
-        else idx = 2;
+        else if (picks[2] > 0) idx = 2;
         
         picks[idx]--;
-        
         answer += tired[idx][0] * d;
         answer += tired[idx][1] * i;
         answer += tired[idx][2] * s;
-    })
+    });
+    
     
     return answer;
 }
