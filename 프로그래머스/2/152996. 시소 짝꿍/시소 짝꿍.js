239f143dd174c731eeac245bc9b1ab3f437589e0
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-17 13:40:34 +0900

    [level 2] Title: 시소 짝꿍, Time: 47.50 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -0,0 +1,15 @@
+function solution(weights) {
+    
+    weights = weights.sort((a, b) => b - a);
+    const dict = {};
+    
+    return weights.reduce((cnt, w) => {
+        if (dict[w]) cnt += dict[w];
+        if (dict[w * 2]) cnt += dict[w * 2];
+        if (dict[w * 3 / 2]) cnt += dict[w * 3 / 2];
+        if (dict[w * 4 / 3]) cnt += dict[w * 4 / 3];
+        
+        dict[w] = (dict[w] || 0) + 1;
+        return cnt;
+    }, 0);
+}
