f08afca0d520dc109a61b1a443dcc5a3da2dc3af
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-18 09:24:20 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 12.36 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,40 +1,34 @@
-// 문제요약: k를 만족하는 가장 짧은 부분수열의 두 인덱스를 구하기
-// 알고리즘 선택:
-//  만약 첫번째 인덱스부터 k를 만족할때까지 검사한다면 시간초과가 발생할것임
-//      O(1000000000000)
-//  최소 부분수열 구하기이므로 최적화문제, dp 고려
-//  선택할 수 있는 건 두 가지
-//      다음 수를 선택=>부분수열 이어가기
-//      다음 수를 선택하지 않음=>부분수열 끝
-//  하지만 경로최적화 문제가 있음. 예를 들어 특정 수 x에 도달하기 위해선 다른 여러 경로에서 접근할 수 없음 => dp 안됨
-//  부분수열은 중간에 끊어질 수 없다는 특징이 있음
-//  두 개의 포인터를 만들고 k를 만족할때까지 두 개의 포인터를 이동시키는 방법
-// 부분문제 분해: 
-//  왼쪽, 오른쪽=0 으로 시작
-//  만약 sum이 k보다 크다면, 왼쪽++
-//  만약 sum이 k보다 작다면, 오른쪽++
-//  같으면 갱신 후 오른쪽++
+// 문제요약: 합이 k인 가장 짧은 부분수열의 양끝 인덱스 구하기
+// 알고리즘 선택: 
+//  상태: 시작, 길이, 합
+//  dp[시작][길이] = 합: 시작이 i이고, 길이가 j일 때 합
+//  부분문제가 최적해가 될 수 있나?
+//      선택할 수 있는 건 현재 수를 이전과 잇기, 또는 끊고 새로 시작하기
+//      만약 두 경우 모두 k보다 작다면 최적값을 결정할 수 없음. 
+//          왜냐하면 뒤에 뭐가 나오느냐에 따라 달라지기 때문
+//  투포인터
+//  인덱스를 하나씩 옮기면서 k보다 클 경우 왼쪽인덱스, 작을 경우 오른쪽 인덱스
 
 function solution(sequence, k) {
     var answer = [];
-    let diff = Infinity;
     
-    let lIdx = 0;
+    let v = Infinity;
+    let lIdx = 0; 
     let rIdx = 0;
-    let sum = 0; 
+    let sum = 0;
     
     while (rIdx <= sequence.length) {
-        if (sum === k) {
-            if ((rIdx - lIdx) < diff) {
-                diff = rIdx - lIdx;
+        if (sum < k) sum += sequence[rIdx++];
+        else if (sum > k) sum -= sequence[lIdx++];
+        else {
+            const diff = rIdx - lIdx;
+            if (diff < v) {
+                v = diff;
                 answer = [lIdx, rIdx];
             }
             sum += sequence[rIdx++];
-        } else if (sum < k) sum += sequence[rIdx++];
-        else sum -= sequence[lIdx++];
+        }
     }
-    
     answer[1]--;
-    
     return answer;
 }
