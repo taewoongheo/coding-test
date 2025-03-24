738ff531f36fc72364e1a3e59f0d15e7bd743aa3
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-03-24 16:32:16 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 718.00 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
@@ -0,0 +1,16 @@
+function solution(n, l, r) {
+    var answer = 0;
+    
+    function check(num) {
+        if (num < 5 && num !== 2) return true;
+        if ((num - 2) % 5 === 0) return false;
+        
+        return check(Math.floor(num / 5));
+    }
+    
+    for (let i = l - 1; i < r; i++) {
+        if (check(i)) answer++;
+    }
+    
+    return answer;
+}
