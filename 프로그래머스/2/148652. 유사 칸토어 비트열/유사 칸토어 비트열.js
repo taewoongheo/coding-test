2b974c39b358c647a55d33949ecdfb77e0035e4b
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-04-03 09:03:32 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 717.32 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
@@ -1,18 +1,15 @@
-// 현재 위치가 어디서 왔는지 거슬러 올라가면 된다. 
-// 하나 당 x5개씩 늘어나니까, 5로 나눈 몫이 근원지가 됨
-// 이때 11011 이므로, 2번째면 false
-
 function solution(n, l, r) {
     var answer = 0;
     
     function check(num) {
         if (num < 5 && num !== 2) return true;
+        
         if ((num - 2) % 5 === 0) return false;
-       
+        
         return check(Math.floor(num / 5));
     }
     
-    for (let i = l - 1; i <= r - 1; i++) {
+    for (let i = l - 1; i < r; i++) {
         if (check(i)) answer++;
     }
     
