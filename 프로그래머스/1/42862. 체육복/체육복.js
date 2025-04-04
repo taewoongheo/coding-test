e679ced8d58b6b1d2272207f8faf345618d0d3c3
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-04-04 09:35:12 +0900

    [level 1] Title: 체육복, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,24 +1,14 @@
-// 문제: 여벌의 옷을 빌려줄 때 체육복을 가지는 최대 학생 수
-// 여분이 있는 학생은 바로 앞 뒤로 한번만 빌려줄 수 있음
-// 체육복이 없는 학생들은 set 에 넣고, 여분의 체육복이 있는 학생들은 자신의 앞 학생부터 검사
-// 만약 자신의 앞이 체육복이 없는 학생이라면 그 학생을 set에서 없애고 다음으로 넘어감
-// 뒤도 검사
-// 앞에서부터 채우기
-
 function solution(n, lost, reserve) {
-    
     reserve = reserve.sort((a, b) => a - b);
-    const s = new Set([...lost]);
-
-    for (let i = 0; i < reserve.length; i++) {
-        if (s.has(reserve[i])) { 
-            s.delete(reserve[i])
-            reserve.splice(i, 1);
-            i--; 
-        }
-    }
     
-    console.log(reserve);
+    const s = new Set([...lost]);
+    reserve = reserve.reduce((reserve, cur) => {
+        if (s.has(cur)) {
+            s.delete(cur);
+            return reserve;
+        }
+        return [...reserve, cur];
+    }, []);
     
     for (const r of reserve) {
         if (s.has(r - 1)) {
