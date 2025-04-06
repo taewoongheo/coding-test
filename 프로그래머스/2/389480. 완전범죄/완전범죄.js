6c011541c3e12274fcab085b265e53fcbd47babc
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-04-06 10:17:53 +0900

    [level 2] Title: 완전범죄, Time: 1.64 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,51 +1,43 @@
-// 문제요약: A,B 도둑이 모든 물건을 훔칠 때, A도둑의 흔적이 최솟값 구하기
-// 알고리즘 선택: 
-//  일단 모든 물건을 훔치려면 B가 모든 물건을 갖고 있는 상태에서 A,B 조건이 만족하도록 A의 개수를 조정하면 됨
-//  B가 모든 물건을 훔친 상태에서 A의 조합을 구해 그 수만큼 빼고 값을 갱신하는 방법
-//      하나의 물건을 A가 선택하거나, 선택하지 않을 수 있으므로 O(40!)의 시간복잡도를 가짐
-//      이때 i 번째 물건을 고를지 말지 선택할 때, 이전에 고른 물건의 상태가 중복됨
-//  i번째 물건을 선택할 지 말지 고르는 것은 결정문제임
-//  또한 i번째 물건을 선택할 때 이전까지의 물건의 선택이 중복됨
-//  물건을 선택하는 각 상태가 부분문제의 최적해가 될 수 있음(A의 최솟값이 최적해)
-//  점화식 세우기
-//      상태: 현재 고를 물건, A의 흔적, B의 흔적
-//      구해야 하는 최솟값은 A의 흔적, A의 작을 수록 최적해
-//      dp[현재고를물건][B의흔적]=A의흔적
-//      두 가지 선택이 가능
-//          현재 물건을 A가 선택
-//              dp[i][b흔적]=min(dp[i-1][b흔적]+a흔적, )
-//          현재 물건을 B가 선택
-//              dp[i][b흔적+현재물건의 b흔적]=dp[i-1][b흔적]
-//                  방향자체가 왼쪽에서 오른쪽으로 정해져있기 때문에 최솟값 갱신 필요없이 바로 값으로 넣으면 됨
-// 부분문제 분해: 
-//  dp[물건개수+1][m]
-//  dp[0][0]=0 물건을 가지지 않은 상태로 초기화
-//  for 1~i in i:
-//      a, b = info[i - 1]
-//      for 0~m in j:
-//          dp값 갱신
+// 문제: A,B 도둑이 모든 물건을 훔쳤을 때, A도둑이 남긴 흔적의 누적 개수의 최솟값 구하기
+// 최솟값 문제이므로 dp 고려
+// 상태: A, B 도둑의 누적 흔적, 현재 물건
+// dp[현재물건][B도둑의 흔적]=A도둑의 흔적, 현재물건이 i이고, B도둑의 흔적이 j일 때 A도둑의 흔적
+//  A도둑의 흔적이 최솟값인 부분문제를 선택하면 전체문제의 최적해가 되므로 부분문제의 최적해조건만족
+//  이전상태에서의 선택과 독립적이다. 왜냐하면 물건 i를 선택할 때 i-1의 선택에 영향을 받지 않기 때문
+//      즉, 특정 해에 도달하기까지 여러 경로가 존재하므로 각 부분문제는 독립적
+// 점화식
+//  dp[현재물건][B도둑의 흔적]=A도둑의 흔적
+//  두 가지 선택이 가능
+//      1. 현재 물건을 A도둑이 훔침
+//          dp[현재물건][B도둑흔적]=A도둑 흔적 + 물건 흔적
+//      2. 현재 물건을 B도둑이 훔침
+//          dp[현재물건][B도둑흔적 + 물건흔적]=A도둑 흔적
 
 function solution(info, n, m) {
-    var answer = 0;
+    var answer = Infinity;
     
     const dp = Array.from({length: info.length + 1}, () => 
                          Array.from({length: m}, () => Infinity));
     dp[0][0] = 0;
     
-    for (let i = 1; i <= info.length; i++) {
-        const [a, b] = info[i - 1];
-        for (let j = 0; j < m ; j++) {
-            dp[i][j] = Math.min(dp[i - 1][j] + a, dp[i][j]);
+    for (let i = 0; i < dp.length - 1; i++) {
+        const [a, b] = info[i];
+        for (let j = 0; j < dp[i].length; j++) {
+            if (dp[i][j] === Infinity) continue;
             
-            if (j + b >= m) continue;
-            dp[i][j + b] = dp[i - 1][j];
+            // A가 선택
+            if (dp[i][j] + a < n) dp[i + 1][j] = Math.min(dp[i][j] + a, dp[i + 1][j]);
+            
+            // B가 선택
+            if (j + b < m) dp[i + 1][j + b] = Math.min(dp[i][j], dp[i + 1][j + b]);
         }
     }
     
-    answer = dp[dp.length - 1].reduce((answer, cur) => {
-        if (cur < n) return Math.min(answer, cur);
-        return answer;
-    }, Infinity);
+    for (let i = 0; i < dp.at(-1).length; i++) {
+        answer = Math.min(answer, dp.at(-1)[i]);
+    }
     
-    return answer === Infinity ? -1 : answer;
+    if (answer === Infinity) return -1;
+    
+    return answer;
 }
