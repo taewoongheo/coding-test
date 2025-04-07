30554fcb8a9b4e5a3f8e6d0e8c64ca0863d4cc28
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-04-07 09:21:11 +0900

    [level 2] Title: 서버 증설 횟수, Time: 32.15 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
@@ -1,39 +1,29 @@
-// 알고리즘 요약: 서버 한 대가 m명 미만까지 수용 가능하고, k동안 유지될 때 최소 증설 횟수 구하기
-// 알고리즘 선택: 
-//  단순구현
-// 부분문제 분해: 
-//  0/3=0
-//  1/3=0
-//  2/3=0
-//  3/3=1
-//  4/3=1
-//  10/3=3
-//  현재 게임 이용자의 수를 이용해 필요한 서버의 개수를 구함=Math.floor(p/m);
-//  현재 서버개수와 필요한 서버의 개수를 비교하고 부족하다면 추가
-//  마지막에 서버의 타이머를 1씩 증가하고 k를 넘으면 제거
+// 문제: 총 서버 증설 횟수 구하기
+// 서버 객체를 만들어 놓고, 그 수와 비교하면서 서버를 추가
 
 function solution(players, m, k) {
     var answer = 0;
     
-    let server = [];
-    for (let i = 0; i < players.length; i++) {
-        const servercnt = server.length;
-        const playercnt = players[i];
-        const need = Math.floor(playercnt / m);
+    let servers = [];
+    for (const player of players) {
+        const need = Math.floor(player / m);
+        const servercnt = servers.length;
         
-        if (servercnt < need) {
-            for (let j = 0; j < need-servercnt; j++) {
-                server.push(1);
+        if (need > servercnt) {
+            for (let j = 0; j < need - servercnt; j++) {
+                servers.push({timer: k});
                 answer++;
             }
         }
         
-        server = server.reduce((server, el) => {
-            if (el + 1 > k) return server;
-            server.push(el + 1);
-            return server;
-        }, []);
-
+        const newServerStatus = [];
+        for (const server of servers) {
+            server.timer--;
+            if (server.timer === 0) continue;
+            newServerStatus.push(server);
+        }
+        
+        servers = [...newServerStatus];
     }
     
     return answer;
