f2a6993ca003934a3733cce8a428b23e74172a5c
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-04-09 09:17:07 +0900

    [level 2] Title: 비밀 코드 해독, Time: 235.31 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.js
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.js
@@ -1,40 +1,47 @@
+// 문제: 비밀코드로 가능한 조합의 개수를 구하기
+// 오름차순 조합을 모두 구한 뒤, q, ans 와 일치하는 조합의 개수를 세기
+// 5자리, 1~30 => 30!/25!x5!
+
 function solution(n, q, ans) {
     var answer = 0;
     
-    const arr = Array.from({length: n}, (_, idx) => idx + 1);
-    const combinations = getCombinations(arr, 5);
-
+    const combinations = getCombination(Array.from({length: n}, (_, idx) => idx + 1), 5);
+    
     for (let i = 0; i < combinations.length; i++) {
         let flag = true;
-        const combi = combinations[i];
+        
         for (let j = 0; j < q.length; j++) {
-            const cnt = matchCnt(combi, q[j]);
-            if (cnt !== ans[j]) {
+            if (!check(combinations[i], q[j], ans[j])) {
                 flag = false;
-                break;
-            };
+                continue;
+            }
         }
+        
         if (flag) answer++;
     }
     
-    function matchCnt(combi, q) {
+    function check(combi, q, ans) {
         let cnt = 0; 
-        for (let i = 0; i < combi.length; i++) {
-            if (q.includes(combi[i])) cnt++;
+        
+        for (let i = 0; i < q.length; i++) {
+            if (combi.includes(q[i])) cnt++;
         }
-            
-        return cnt;
+        
+        if (cnt === ans) return true;
+        
+        return false;
     }
     
-    function getCombinations(arr, cnt) {
+    function getCombination(arr, cnt) {
         const res = [];
         
         if (cnt === 1) return arr.map(el => [el]);
-        arr.forEach((num, idx, origin) => {
-            const rest = origin.slice(idx + 1);
-            const attached = getCombinations(rest, cnt - 1);
-            const at = attached.map(combi => [num, ...combi]);
-            res.push(...at);
+        
+        arr.forEach((item, idx, origin) => {
+            const rest = arr.slice(idx + 1);
+            const combinations = getCombination(rest, cnt - 1);
+            const attached = combinations.map(combi => [item, ...combi]);
+            res.push(...attached);
         });
         
         return res;
