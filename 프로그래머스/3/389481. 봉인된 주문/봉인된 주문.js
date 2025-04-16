49fe0edc8b3658ec868d15e798554d22f58b2a6d
Taewoong Heo <127009780+taewoongheo@users.noreply.github.com>
2025-04-16 09:12:47 +0900

    [level 3] Title: 봉인된 주문, Time: 575.39 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
@@ -1,27 +1,4 @@
-// 문제요약: 봉인된 주문을 제외했을 때 n 번째 주문 찾기
-// 알고리즘 선택: 
-//  a~z를 사용하는 26진수
-//  문자열->숫자, 숫자->문자열
-// 부분문제 분해: 
-//  bans 를 내림차순 정렬
-//  pop 하면서 n 보다 작은 봉인된 문자열이 있다면, n++
-//  만약 더 작은 문자열이 나온다면 break 후 n을 문자열로 바꿔서 바로 출력
-
 function solution(n, bans) {
-    var answer = '';
-    
-    bans = bans.sort((a, b) => strToNum(b) - strToNum(a));
-    
-    console.log(numToStr(n))
-    console.log(bans)
-    while (bans.length) {
-        if (n >= strToNum(bans.pop())) {
-            n++;
-            continue;
-        };
-        
-        break;
-    }
     
     function strToNum(str) {
         let num = 0;
@@ -43,6 +20,13 @@ function solution(n, bans) {
         }
         
         return str;
+    }
+    
+    bans = bans.sort((a, b) => strToNum(b) - strToNum(a));
+    
+    while (bans.length) {
+        if (strToNum(bans.pop()) <= n) n++;
+        else break;
     }
     
     return numToStr(n);
