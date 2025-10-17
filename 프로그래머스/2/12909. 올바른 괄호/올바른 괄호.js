43673f2e47d3f343b281530259ff1b905c234f9f
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-10-18 00:38:58 +0900

    [level 2] Title: 올바른 괄호, Time: 29.10 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,17 +1,24 @@
-function solution(s){
+// stack
+// '(' 가 들어오면 push
+// ')' 가 들어왔을 때 스택이 비어있으면 false, 비어있지 않다면 pop
 
+function solution(s){
+    const arr = s.split('');
     const stack = [];
     
-    for (const char of s) {
-        
-        if (char === ')') {
-            if (stack.pop() !== '(') return false;
-        } else {
-            stack.push(char);
+    for (let i = 0; i < arr.length; i++) {
+        const el = arr[i];
+        if (el === '(') {
+            stack.push('(');
+            continue;
         }
+        
+        if (!stack.length) {
+            return false;
+        }
+        
+        stack.pop();
     }
     
-    if (stack.length) return false;
-
-    return true;
+    return !stack.length;
 }
