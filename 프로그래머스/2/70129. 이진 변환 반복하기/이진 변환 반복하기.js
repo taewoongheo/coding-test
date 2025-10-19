6751137bf386a28c178af26cf2ede88e20add312
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-10-19 16:00:52 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 15.98 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,29 +1,10 @@
 function solution(s) {
     var answer = [0, 0];
     
-    const numToBin = (len, cnt) => {
-        s = '';
-        let num = len - cnt;
-        
-        while (num !== 0) {
-            s = (num % 2).toString() + s;
-            num = Math.floor(num / 2);
-        }
-        
-        return s;
-    }
-    
     while (s !== '1') {
-        let len = s.length;
-        let cnt = 0;
-        for (let i = 0; i < len; i++) {
-            if (s[i] === '0') cnt++;
-        }
-        
-        s = numToBin(len, cnt);
-        
         answer[0]++;
-        answer[1] += cnt;
+        answer[1] += (s.match(/0/g) || []).length;
+        s = s.replace(/0/g, "").length.toString(2);
     }
     
     return answer;
