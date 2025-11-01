d24d12ea7fa4abdf480b9aeeb4391984cba02e9a
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-11-01 21:37:47 +0900

    [level 2] Title: 튜플, Time: 36.16 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,36 +1,7 @@
 function solution(s) {
-    let ans = [];
-    
-    const strArr = s.split(',');
-    const arr = [];
-    let stack = [];
-    for (let i = 0; i < strArr.length; i++) {
-        const char = strArr[i];
-        const p1 = char.replaceAll(/{/g, '');
-        
-        if (p1.includes('}')) {
-            const p2 = p1.replaceAll(/}/g, '');
-            arr.push([...stack, p2]);
-            stack = [];
-            continue;
-        }
-        
-        stack.push(p1);
-    }
-    
-    arr.sort((a, b) => a.length - b.length);
-    
-    const set = new Set();
-    for (let i = 0; i < arr.length; i++) {
-        const str = arr[i];
-        
-        for (let j = 0; j < str.length; j++) {
-            if (!set.has(str[j])) {
-                set.add(str[j]);
-                ans.push(Number(str[j]));
-            }
-        }
-    }
-    
-    return ans;
+    return (JSON.parse(s.replaceAll(/{/g, '[').replaceAll(/}/g, ']')))
+        .sort((a, b) => a.length - b.length)
+        .reduce((acc, cur, idx) => {
+            return acc.concat(cur.filter(el => !acc.includes(el)));
+        })
 }
