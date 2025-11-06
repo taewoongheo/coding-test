d4dfb700ab5af0d538d5bda8f71a8d411e9cb876
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-11-06 16:08:26 +0900

    [level 2] Title: 의상, Time: 6.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.44 ms
+메모리: 33.5 MB, 시간: 6.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 30일 14:32:29
+2025년 11월 06일 16:08:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,9 @@
 function solution(clothes) {
-    let ans = 0;
-    
-    const c = clothes.reduce((obj, cur) => {
-        const [_, name] = cur;
-        if (!obj[name]) obj[name] = 1;
-        obj[name]++;
-        return obj;
+    const obj = clothes.reduce((o, c) => {
+        o[c[1]] = o[c[1]] + 1 || 2;
+        return o;
     }, {});
     
-    const arr = Object.keys(c);
-    
-    return arr.reduce((acc, cur) => acc * c[cur], 1) - 1;
+    console.log(Object.entries(obj));
+    return Object.entries(obj).reduce((acc, cur) => acc * cur[1], 1) - 1;
 }
