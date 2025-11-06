c3ab2c262ebda8b330f49bbf400bb88b8246ef80
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-11-06 16:30:12 +0900

    [level 2] Title: [1차] 캐시, Time: 69.95 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,29 +1,24 @@
 function solution(cacheSize, cities) {
-    let ans = 0; 
-    
-    // 캐시를 검사
-    // hit 시, 순위를 올리고 ans += 1
-    //  순위올리기=기존 요소 없애고 재추가
-    // miss 시, 캐시에 추가하고 ans += 5
-    // 캐시 크기를 검사해서 초과 시 삭제
-    
     const cache = new Map();
     
-    cities.forEach(city => {
-        const paresdCity = city.toLowerCase();
-        if (cache.has(paresdCity)) {
-            ans += 1;
-            cache.delete(paresdCity);
-            cache.set(paresdCity, paresdCity);
-        } else {
-            ans += 5;
-            cache.set(paresdCity, paresdCity);
-        }
+    let ans = 0;
+    for (const city of cities) {
+        const pCity = city.toUpperCase();
         
-        if (cache.size > cacheSize) {
-            cache.delete(cache.keys().next().value);
-        }
-    });
+        if (cache.get(pCity)) {
+            // cache hit
+            ans += 1;
+            cache.delete(pCity);
+            cache.set(pCity, pCity);
+            continue;
+        } 
+        
+        // cache miss
+        ans += 5;
+        cache.set(pCity, pCity);
+
+        if (cache.size > cacheSize) cache.delete(cache.keys().next().value);
+    }
     
     return ans;
 }
