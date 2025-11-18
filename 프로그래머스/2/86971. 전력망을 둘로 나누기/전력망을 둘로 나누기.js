61bf95377cb5fc123e6f19c1d25d69d1d9f0d785
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-11-18 14:53:37 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.27 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,55 +1,46 @@
-// 문제요약: 트리를 두 서브트리로 나눌 때, 두 서브트리의 차가 가장 적은 경우를 구하기
-// 입력: 
-//  n: 송전탑의 개수
-//  wires: 전선 정보
-// 출력: 
-//  두 서브트리의 차가 최소인 경우의 값
-// 조건: 
-//  2 <= n <= 100 
-// 알고리즘 선택: 
-//  모든 간선에 대해 한번씩 끊어보고, 각 간선의 시작점을 기준으로 부분트리의 개수 세기
-//  차가 최소인 경우를 갱신
-//  입력은 항상 트리 형태이기 때문에 순회하는 경우는 없음
-// 부분문제 분할:
-//  트리를 완성
-//  시작점을 기준으로 트리의 노드 개수를 반환하는 함수를 각 간선별로 실행
-//  간선을 끊는다 = 인접한 노드를 방문배열에 넣음
-
-function treeCnt(tree, node, v, cnt) {
-    childCnt = 1;
-    
-    for (const child of tree.get(node)) {
-        if (!v.has(child)) {
-            v.add(child);
-            childCnt += treeCnt(tree, child, v, cnt + 1);
-        }
-    }
-    
-    return childCnt;
-}
+// 끊을 와이어 하나를 정하고 매번 전부 세는 방법
+//  100개의 송전탑이면, 99개의 와이어가 존재
+//  와이어 하나를 끊고 송전탑 개수를 전부 센다고 하면, 99x100=9900, 시간초과 안남. 가능
 
 function solution(n, wires) {
-    var answer = Infinity;
-    
     const tree = new Map();
-    for (let i = 0; i < wires.length; i++) {
-        const [s, e] = wires[i];
-        if (!tree.has(s)) tree.set(s, []);
-        if (!tree.has(e)) tree.set(e, []);
-        tree.get(s).push(e);
-        tree.get(e).push(s);
+    for (const wire of wires) {
+        const [n1, n2] = wire;
+
+        if (!tree.get(n1)) tree.set(n1, []);
+        if (!tree.get(n2)) tree.set(n2, []);
+        
+        const arr1 = tree.get(n1);
+        const arr2 = tree.get(n2);
+        
+        tree.set(n1, [...arr1, n2]);
+        tree.set(n2, [...arr2, n1]);
     }
     
-    for (let i = 0; i < wires.length; i++) {
-        const [node1, node2] = wires[i];
-        const v1 = new Set([node1, node2]);
-        const v2 = new Set([node1, node2]);
+    const cnt = (node, visit) => {
+        const arr = tree.get(node);
         
-        const subtree1Cnt = treeCnt(tree, node1, v1, 1);
-        const subtree2Cnt = treeCnt(tree, node2, v2, 1);
+        let total = 1;
         
-        answer = Math.min(answer, Math.abs(subtree1Cnt - subtree2Cnt));
+        for (const n of arr) {
+            if (!visit.has(n)) {
+                visit.add(n);
+                total += cnt(n, visit);
+            }
+        }
+        
+        return total;
     }
     
-    return answer;
+    let ans = Infinity;
+    
+    for (const wire of wires) {
+        const [n1, n2] = wire;
+        const cnt1 = cnt(n1, new Set([n1, n2]));
+        const cnt2 = cnt(n2, new Set([n1, n2]));
+        
+        ans = Math.min(Math.abs(cnt1 - cnt2), ans);
+    }
+    
+    return ans;
 }
