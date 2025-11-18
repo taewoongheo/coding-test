e5d9583b63e8432e36531afa18124fe89c5e021d
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-11-18 15:45:02 +0900

    [level 2] Title: 호텔 대실, Time: 26.98 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,54 +1,33 @@
-// 문제요약: 손님들을 받을 수 있는 최소 객실 수 구하기
-// 알고리즘 선택: 
-//  구현
-//  시각을 분 단위로 바꾼 뒤, 시작 시각을 기준으로 오름차순 정렬
-//  예약 시각을 하나씩 받으면서 시간 갱신
-// 부분문제 분해: 
-//  시각->분단위
-//      이때 종료시각에는 +10분
-//  첫번쨰 예약: book_time.pop()
-//  for book_time
-//      두번째 예약: book_time.pop()
-//      현재 사용중인 방 중 두번째 예약 시작 시각보다 작거나 같은 방들을 모두 제거
-//      두번째 예약 방 추가
-//      배열 최댓값 길이 갱신
+// 시작시각 기준으로 정렬
+// 예약 시간이 하나씩 들어옴
+// 현재 사용중인 방 루프를 돌면서 새로 들어온 예약 시간의 시작시각보다 작은 것들은 제거
+// 방을 현재 사용중인 방 배열에 추가, 배열의 길이 최솟값 갱신
+//  방 최대 개수 1000, 최악의 경우 모든 방이 23:59까지라면 빠지는 방이 없으므로 1000x1000=1000000 가능
 
 function solution(book_time) {
-    var answer = 1;
-    
-    book_time = book_time.map(el => [
-        timeToMinute(el[0]), timeToMinute(el[1]) + 10
-    ]);
-    
-    book_time.sort((a, b) => b[0] - a[0]);
-    
-    let status = [book_time.pop()[1]];
-    
-    while (book_time.length) {
-        
-        const next = book_time.pop();
-        const nextStartTime = next[0];
-        const nextEndTime = next[1];
-        
-        const newStatus = [];
-        for (let i = 0; i < status.length; i++) {
-            if (status[i] > nextStartTime) newStatus.push(status[i]);
-        }
-        newStatus.push(nextEndTime);
-        status = [...newStatus];
-        
-        answer = Math.max(answer, status.length);
-    }
-    
-    function timeToMinute(time) {
-        let minute = 0;
-        
+    const t = (time) => {
         const [h, m] = time.split(':').map(Number);
-        minute += h * 60;
-        minute += m;
-        
-        return minute;
+        return h * 60 + m;
     }
     
-    return answer;
+    const b = book_time.map(el => [t(el[0]), t(el[1])]).sort((a, b) => {
+        if (a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0];
+    });
+    
+    let ans = 0;
+    let current = [];
+    for (const book of b) {
+        const [s, e] = book;
+        
+        const newCurrent = [];
+        for (const c of current) {
+            const [cs, ce] = c;
+            if (s < ce) newCurrent.push([cs, ce]);
+        }
+        current = [...newCurrent, [s, e + 10]];
+        ans = Math.max(current.length, ans);
+    }
+    
+    return ans;
 }
