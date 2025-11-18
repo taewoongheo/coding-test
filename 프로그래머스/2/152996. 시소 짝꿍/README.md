81f2f92fb76726fa9205ae6fce96f4df88752a93
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-11-18 14:01:18 +0900

    [level 2] Title: 시소 짝꿍, Time: 18.05 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/README.md
+++ 프로그래머스/2/152996. 시소 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 2] 시소 짝꿍 - 152996 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996#) 
 
 ### 성능 요약
 
-메모리: 41.8 MB, 시간: 58.31 ms
+메모리: 38.5 MB, 시간: 18.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 18일 09:39:03
+2025년 11월 18일 14:01:15
 
 ### 문제 설명
 
--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,15 +1,50 @@
+// 하나 잡고 나머지들이랑 전부 비교하면 100000x100000=10000000000 시간초과
+// 정렬하고 자신보다 큰 애들이랑만 비교하면 최악의 경우 1~100000 더하기=100000x99999/2=9999900000/2 시간초과
+//  근데 최대 길이보다 weights 의 범위가 더 작음. 즉, 최악의 경우에도 항상 중복이 존재
+//  중복을 고려해서 최악의 경우의 수는 중복이 최소 100 개 존재(=weigths 가 1000개의 종류라면, 최대길이 100000에서 중복이 100개 존재)
+//  즉, 중복을 제외하고 비교하면 1~1000 더하기 = 1000x999/2=999000/2 가능
+
 function solution(weights) {
-    weights.sort((a, b) => b - a);
+    const obj = new Map();
+    for (const weight of weights) {
+        const cnt = obj.get(weight);
+        if (!cnt) {
+            obj.set(weight, 1);
+            continue;
+        }
+        obj.set(weight, cnt + 1);
+    }
     
-    const dict = {};
-    return weights.reduce((cnt, w) => {
-        if (dict[w]) cnt += dict[w];
-        if (dict[w * 2]) cnt += dict[w * 2];
-        if (dict[w * 3 / 2]) cnt += dict[w * 3 / 2];
-        if (dict[w * 4 / 3]) cnt += dict[w * 4 / 3];
-        
-        dict[w] = (dict[w] || 0) + 1;
-        
-        return cnt;
-    }, 0);
+    const arr = [];
+    for (const key of obj.keys()) {
+        arr.push({weight: key, cnt: obj.get(key)});
+    }
+
+    arr.sort((a, b) => a.weight - b.weight);
+    
+    let ans = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const sw = arr[i].weight;
+        const sc = arr[i].cnt;
+        // 자기자신
+        if (sc > 1) {
+            ans += (sc * (sc - 1)) / 2;
+        }
+        for (let j = i + 1; j < arr.length; j++) {
+            // 4:2, 4:3, 3:2
+            const bw = arr[j].weight;
+            const bc = arr[j].cnt;
+            if (sw * 4 === bw * 2) {
+                ans += sc * bc;
+            } 
+            if (sw * 4 === bw * 3) {
+                ans += sc * bc;
+            }
+            if (sw * 3 === bw * 2) {
+                ans += sc * bc;
+            }
+        }
+    }
+    
+    return ans;
 }
