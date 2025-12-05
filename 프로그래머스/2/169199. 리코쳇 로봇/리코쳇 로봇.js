f0c0ee6d9e42852857e9af3d1eb4e022aa56a588
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-12-05 15:08:20 +0900

    [level 2] Title: 리코쳇 로봇, Time: 5.47 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -1,99 +1,53 @@
-// 문제요약: R에서 G로 이동하는 최소 횟수. 이때 이동은 가장자리에 닿을 때까지 이동(상하좌우)
-// 알고리즘 선택: 
-//  최단거리 찾기 => bfs
-//  특정 위치에 도달했을 때, 순서 같은 상태는 중요하지 않으므로 단순 bfs로 가능
-// 부분문제 분할:
-//  bfs
-//  move(r, c, dir): 현재 위치에 특정 방향으로 이동한 결과의 좌표를 리턴
-
-class Node {
-    constructor(row, col, cnt) {
-        this.row = row;
-        this.col = col;
-        this.cnt = cnt;
-        this.next = null;
-    }
-}
-
-class Queue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-    
-    push(node) {
-        if (this.head === null) {
-            this.head = node;
-        } else {
-            this.tail.next = node;
-        }
-        this.tail = node;
-        this.length += 1;
-    }
-    
-    pop() {
-        const ret = this.head;
-        this.head = this.head.next;
-        this.length -= 1;
-        return ret;
-    }
-}
-
 function solution(board) {
-    var answer = -1;
+    const map = board.map(el => el.split(''));
     
-    const maxr = board.length;
-    const maxc = board[0].length;
+    const rlen = map.length;
+    const clen = map[0].length;
     
-    const b = [];
-    const v = Array.from({length: maxr}, () => Array.from({length: maxc}, () => false));
+    let s = null;
+    let e = null;
     
-    let rrow, rcol, grow, gcol;
-    for (let i = 0; i < board.length; i++) {
-        b.push(board[i].split(''));
-        board[i].split('').forEach((el, j) => {
-            if (el === 'R') {
-                rrow = i;
-                rcol = j;
-            }
-            if (el === 'G') {
-                grow = i;
-                gcol = j;
-            }
-        })
-    }
-    
-    const q = new Queue();
-    q.push(new Node(rrow, rcol, 0));
-    v[rrow][rcol] = true;
-    const m = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    
-    console.log(q);
-    while (q.length !== 0) {
-        const node = q.pop();
-        if (node.row === grow && node.col === gcol) {
-            answer = node.cnt;
-            break;
-        }
-        for (let i = 0 ; i < 4; i++) {
-            const [nr, nc] = move(node.row, node.col, m[i]);
-            if (!v[nr][nc]) {
-                v[nr][nc] = true;
-                q.push(new Node(nr, nc, node.cnt + 1));
-            }
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] === 'R') s = [i, j];
+            if (map[i][j] === 'G') e = [i, j];
         }
     }
     
-    function move(row, col, dir) {
-        let nrow = row;
-        let ncol = col;
-        while (nrow >= 0 && nrow < maxr && ncol >= 0 && ncol < maxc && b[nrow][ncol] !== 'D') {
-            nrow += dir[0];
-            ncol += dir[1];
+    const q = [[...s, 0]];
+    const v = new Set([`${s[0]}-${s[1]}`]);
+    const d = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    
+    const move = (r, c, d) => {
+        let i = 0;
+        while (true) {
+            let nr = r + d[0];
+            let nc = c + d[1];
+            if (nr < 0 || nr >= rlen || nc < 0 || nc >= clen || map[nr][nc] === 'D') break;
+            
+            r = nr;
+            c = nc;
         }
-        return [nrow-dir[0], ncol-dir[1]];
+        
+        return [r, c];
     }
     
-    return answer;
+    while (q.length) {
+        const [r, c, cnt] = q.shift();
+        
+        if (r === e[0] && c === e[1]) {
+            return cnt;
+        }
+        
+        for (let i = 0; i < 4; i++) {
+            const [nr, nc] = move(r, c, d[i]);
+            const key = `${nr}-${nc}`;
+            if (v.has(key)) continue;
+            
+            v.add(key);
+            q.push([nr, nc, cnt + 1]);
+        }
+    }
+    
+    return -1;
 }
