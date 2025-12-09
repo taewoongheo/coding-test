e4f6991902c3b5b7e11333d2a7f368d5d818412b
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-12-09 19:31:38 +0900

    [level 2] Title: [3차] 방금그곡, Time: 5.77 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/README.md
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 방금그곡 - 17683 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17683) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17683#) 
 
 ### 성능 요약
 
-메모리: 38.9 MB, 시간: 47.21 ms
+메모리: 34.4 MB, 시간: 5.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 09일 19:07:22
+2025년 12월 09일 19:31:36
 
 ### 문제 설명
 
--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -1,64 +1,24 @@
 function solution(m, musicinfos) {
-    const music = []; // { name, Set }
-    
-    let mSize = 0;
-    for (let i = 0; i < m.length; i++) {
-        if (m.charAt(i) === '#') continue;
-        mSize++;
-    }
     
     const timeToMin = (time) => {
         const [h, m] = time.split(":").map(Number);
         return h * 60 + m;
     }
     
-    for (let i = 0; i < musicinfos.length; i++) {
-        const [s, e, name, melody] = musicinfos[i].split(',');
+    let melodies = musicinfos.map(el => {
+        const [s, e, name, melody] = el.split(",");
         const diff = timeToMin(e) - timeToMin(s);
-        
-        let mArr = [];
-        for (let j = 0; j < melody.length; j++) {
-            if (melody.charAt(j) === '#') {
-                const pop = mArr.pop();
-                mArr.push(`${pop}#`);
-                continue;
-            }
-            mArr.push(melody.charAt(j));
-        }
-        
-        const set = new Set();
-        for (let j = 0; j <= diff - mSize; j++) {
-            let str = '';
-            for (let k = 0; k < mSize; k++) {
-                str += mArr[(j + k) % mArr.length];
-            }
-            set.add(str);
-        }
-        
-        music.push({name, diff, melodies: set});
-    }
+        let nm = melody.replace(/[A-Z]#/g, m => m[0].toLowerCase());
+        nm = nm.repeat(Math.ceil(diff / nm.length)).substr(0, diff);
+        return `${diff},${name},${nm}`;
+    });
     
-    // TODO: 재생시간 길이 긴 음악 반환 추가
-    let len = 0;
-    let ret = null;
-    for (let i = 0; i < music.length; i++) {
-        const { name, diff, melodies } = music[i];
-        
-        const before = melodies.size;
-        
-        melodies.add(m);
-        
-        const after = melodies.size;
-        
-        if (before === after) {
-            if (diff > len) {
-                len = diff;
-                ret = name;
-            }
-        }
-    }
+    melodies = melodies.sort((a, b) => b.split(',')[0] - a.split(',')[0]);
     
-    if (len) return ret;
+    const nnm = m.replace(/[A-Z]#/g, m => m[0].toLowerCase());
+    const ans = melodies.filter(el => el.split(',')[2].indexOf(nnm) != -1);
     
-    return '(None)';
+    console.log(ans);
+    
+    return ans.length === 0 ? '(None)' : ans[0].split(',')[1];
 }
