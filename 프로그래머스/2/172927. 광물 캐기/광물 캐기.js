5e65d75a84386386248065af0180fa111af9d2fa
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2025-12-30 11:52:02 +0900

    [level 2] Title: 광물 캐기, Time: 0.42 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,62 +1,59 @@
-// 문제요약: 광물을 모두 캐거나 곡괭이를 모두 사용할 때까지 최소 피로도
-// 알고리즘 선택: 
-//  한번 선택한 곡괭이로 5개를 무조건 캐야함
-//  광물을 캐는 순서가 정해져 있음
-//      즉, 광물은 5개씩 그룹화 뒤, 그 순서대로만 캐야함
-//  만약 5개씩 그룹화된 광물을 좋은 곡괭이부터 사용한다면 나중에 피로도가 높아질 수 있음
-//  따라서 광물을 피로도 순서대로 정렬한 뒤, 좋은 곡괭이부터 사용하면 됨
-//  어차피 정렬해도 5개씩만 캐면 된다는 순서만 유지하면 되기 때문에 상관없음
+// 바로 앞 광물만 보고 캐면 뒤에 나올 광물을 모르기 때문에 최적으로 구할 수 없음
+//  바로 앞 5개의 광물만 보고 캐는 경우에도 마찬가지
+// 하나의 곡괭이를 5번씩 쓸 수 있으므로 광물 5개를 하나의 그룹으로 묶음
+//  dia, iron, stone 별 점수를 부여해 각 그룹당 점수를 계산
+//  점수가 높은 그룹부터 정렬한 뒤, 다이아곡괭이부터 사용하면 됨
+
+// 곡괭이 개수 x 5 < 광물 개수인 경우 마지막 남는 광물들은 포함하면 안됨
 
 function solution(picks, minerals) {
-    var answer = 0;
+    let total = picks.reduce((acc, cur) => acc + cur * 5, 0);
     
-    const max = Math.min((picks[0] + picks[1] + picks[2]) * 5, minerals.length);
-    let arr = [];
-    let marr = [0, 0, 0];
-    let cnt = 0;
+    if (total < minerals.length) minerals = minerals.slice(0, total);
     
-    for (let i = 0; i < max; i++) {
-        const m = minerals[i];
-        
-        if (m === 'diamond') marr[0]++;
-        else if (m === 'iron') marr[1]++;
-        else marr[2]++;
-        cnt++;
-        
-        if (cnt === 5) {
-            cnt = 0;
-            arr.push(marr);
-            marr = [0, 0, 0];
-        }
+    const group = [];
+    while (minerals.length >= 5) {
+        group.push(minerals.splice(0, 5));
     }
-    arr.push(marr);
+    group.push(minerals);
     
-    arr = arr.sort((a, b) => {
-        if (a[0] !== b[0]) return b[0] - a[0];
-        else if (a[1] !== b[1]) return b[1] - a[1];
-        return b[2] - a[2];
-    });
+    group.sort((a, b) => getScore(b) - getScore(a));
     
-    const tired = [
-        [1, 1, 1],
-        [5, 1, 1],
-        [25, 5, 1],
-    ]
+    function getScore(arr) {
+        return arr.reduce((acc, cur) => {
+            if (cur === 'diamond') acc += 25;
+            else if (cur === 'iron') acc += 5;
+            else acc += 1;
+            return acc;
+        }, 0);
+    }
     
-    arr.forEach(el => {
-        const [d, i, s] = el;
+    return group.reduce((acc, cur) => {
+        if (picks[0]) {
+            picks[0]--;
+            acc += cur.length;
+        } else if (picks[1]) {
+            picks[1]--;
+            for (const el of cur) {
+                if (el === 'diamond') {
+                    acc += 5;
+                } else {
+                    acc += 1;
+                }
+            }
+        } else if (picks[2]) {
+            picks[2]--;
+            for (const el of cur) {
+                if (el === 'diamond') {
+                    acc += 25;
+                } else if (el === 'iron') {
+                    acc += 5;
+                } else {
+                    acc += 1;
+                }
+            }
+        }
         
-        let idx = 0;
-        if (picks[0] > 0) idx = 0;
-        else if (picks[1] > 0) idx = 1;
-        else if (picks[2] > 0) idx = 2;
-        
-        picks[idx]--;
-        answer += tired[idx][0] * d;
-        answer += tired[idx][1] * i;
-        answer += tired[idx][2] * s;
-    });
-    
-    
-    return answer;
+        return acc;
+    }, 0);
 }
