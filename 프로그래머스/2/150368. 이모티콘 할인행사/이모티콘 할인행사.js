68d4cba6fe46bc5b4226c9b44a9bea5bd046bb64
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2026-01-14 13:44:25 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 194.60 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,55 +1,68 @@
+// 이모티콘 최대 개수 = 7, 각 이모티콘마다 4가지의 할인 = 4^7 = 16384
+// 최대 유저 = 100
+// 16384 x 100 = 1638400 x 4(계산 시 할인된 아이템 루프)
+
 function solution(users, emoticons) {
-    var answer = [0, 0];
+    let ans = [0, 0];
     
-    const sales = [];
     const discount = [10, 20, 30, 40];
-    
-    const arr = Array.from({length: emoticons.length}, () => 0);
-    dfs(arr, 0);
-    
-    for (let i = 0; i < sales.length; i++) {
-        const sale = sales[i];
-        const candi = [0, 0];
-        for (let j = 0; j < users.length; j++) {
-            const [r, p] = users[j];
-            
-            let amount = 0;
-            for (let k = 0; k < sale.length; k++) {
-                if (r <= sale[k]) {
-                    amount += emoticons[k] * (100 - sale[k]) / 100;
-                }
-            }
-            if (amount >= p) {
-                candi[0]++;
-            } else {
-                candi[1] += amount;
-            }
+
+    // emoticons 를 순서대로 탐색한다고 가정
+    const cal = emoticons.map(el => {
+        const obj = {};
+        for (const rate of discount) {
+            obj[rate] = el * ((100 - rate) / 100);
         }
-        
-        
-        if (candi[0] >= answer[0]) {
-            if (candi[0] > answer[0]) {
-                answer = candi;
-            } else {
-                if (candi[1] > answer[1]) {
-                    answer = candi;
-                }
-            }
-        }
-    }
+        return obj;
+    });
     
-    function dfs(ar, idx) {
-        if (idx === emoticons.length) {
-            sales.push([...ar]);
+    // emoticons 를 순서대로 나열해야됨
+    const res = [];
+    
+    const dfs = (depth, arr) => {
+        
+        if (depth === emoticons.length) {
+            res.push([...arr]);
             return;
         }
         
-        for (let i = 0; i < 4; i++) {
-            ar[idx] = discount[i];
-            dfs(ar, idx + 1);
-            ar[idx] = 0;
+        for (const rate of discount) {
+            arr.push(rate);
+            dfs(depth + 1, arr);
+            arr.pop();
         }
     }
     
-    return answer;
+    dfs(0, []);
+    
+    for (const combi of res) {
+        
+        let cnt = 0;
+        let amt = 0;
+        
+        for (const user of users) {
+            const [minimum, threshold] = user;
+            
+            let p = 0;
+            for (let i = 0; i < combi.length; i++) {
+                if (minimum <= combi[i]) {
+                    p += cal[i][combi[i].toString()];
+                }
+            }
+            
+            if (p >= threshold) {
+                cnt++;
+            } else {
+                amt += p;
+            }
+        }
+        
+        if (ans[0] < cnt) {
+            ans = [cnt, amt];
+        } else if (ans[0] === cnt && ans[1] < amt) {
+            ans = [cnt, amt];
+        }
+    }
+    
+    return ans;
 }
