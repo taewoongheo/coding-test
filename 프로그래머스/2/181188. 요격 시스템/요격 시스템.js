3b4f5e6210d326a5231f0a17b89a4aeba8888446
tw.heo <127009780+taewoongheo@users.noreply.github.com>
2026-01-30 13:43:22 +0900

    [level 2] Title: 요격 시스템, Time: 491.07 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,14 +1,12 @@
-// 미사일이 들어왔을 때 바로 터뜨리지 않고 기다렸다가 다른 미사일도 모였을 때 한꺼번에 터뜨리는게 좋음
-// e 기준으로 오름차순 정렬,
-//  다음 미사일의 시작지점 ns < e 라면 제낌
-//  ns > e 라면, 요격 한번 해주고 ns 로 갱신
+// e 기준으로 오름차순 정렬(e 전에는 무조건 터뜨려야 하기 때문)
+//  ns = 다음 시작점
+//  e < ns 면 터뜨리고 e 갱신
 
 function solution(targets) {
-    var answer = 1;
+    let ans = 1;
     
     targets = targets.sort((a, b) => b[1] - a[1]);
     let coor = targets.pop()[1];
-
     
     while (targets.length) {
         const [s, e] = targets.pop();
@@ -16,8 +14,8 @@ function solution(targets) {
         if (s < coor) continue;
         
         coor = e;
-        answer++;
+        ans++;
     }
     
-    return answer;
+    return ans;
 }
